7c7f123c9518547d15c977dd67f2bf273d669f6f
Mmesek <themmesek@gmail.com>
2020-01-12 02:34:44 +0100

    Moar Improvements

--- bot/rss.py
+++ bot/rss.py
@@ -1,20 +1,17 @@
 import feedparser, re, requests, asyncio, time, html2text
 from bs4 import BeautifulSoup as bs
+from math import ceil
 #import bot.utils as utils
 import utils
 
 def parseEntry(self, entry, last, source, feed):
     desc = bs(entry['description'],'html.parser')
-    print(entry['published_parsed'])
-    print(int(time.mktime(entry['published_parsed'])))
-    if int(time.mktime(entry['published_parsed'])) > last:
-        self.db.RSSUpdateLastEntry(source[0], int(time.mktime(entry['published_parsed'])))
     embed = utils.Embed().setColor(source[3]).setTimestamp(time.strftime("%Y-%m-%dT%H:%M:%S",entry['published_parsed']))
     if  '//nitter' in source[1]:
         if entry['author'] == '@DyingLightGame':
             embed.setUrl(entry['link'].replace('nitter.net','twitter.com')).setTitle(feed['title']).setAuthor(entry['author'],feed['link'].replace('nitter.net','twitter.com'),feed['image']['url']).setDescription(entry['title'])
         else:
-            return {"description":"Error: author mismatch"}
+            return []
     else:
         embed.setTitle(entry['title']).setUrl(entry['link'])
     if '//steam' in source[1]:
@@ -38,8 +35,13 @@ def parseEntry(self, entry, last, source, feed):
     links_all = re.findall(r'\((https://store.steam\S*)\)\s',h2tl)
     for link in links_all:
         s = link.split('/')[-2].replace('_',' ')
-        h2tl=h2tl.replace(f"({s})[{link}]","")
-        embed.addField("Steam Store",f"[{s}]({link})")
+#        print(s,link)
+        h2tl=h2tl.replace(f"[\n","[").replace(f"[{link}]({link})","")
+        embed.addField("Steam Store",f"[{s}]({link})",True)
+    events = re.findall(r'\((\S*/partnerevents/view/\S*)\)\s',h2tl)
+    for link in events:
+        h2tl=h2tl.replace(f"[\n","[").replace(f"[{link}]({link})","")
+        embed.addField("Steam Event",f"[View Event]({link})",True)
     images = re.findall(r'\!\[\]\(\S*\)',h2tl)
     for image in images:
         h2tl=h2tl.replace(f"{image}",'')
@@ -53,50 +55,53 @@ def parseEntry(self, entry, last, source, feed):
     else:
         ftext = source[0]
     if '//nitter' not in source[1]:
-        embed.setFooter('',ftext).setDescription(desc.replace(' * ','\n'))
+        embed.setFooter('',ftext).setDescription(desc.replace(' * ','\n').replace('______','-')[:2023])
     if '//steam' in source[1] or '//nitter' in source[1]:
         embed.setImage(imag)
     else:
         embed.setThumbnail(imag)
-    #print(embed.embed)
-    return embed.embed
+    return [embed.embed]
 
 async def rss(self, language):
     feeds = self.db.RSSGetSources(language)
-    embeds = []
     for source in feeds:
-    #    print(source)
+        embeds = []
         feed = feedparser.parse(source[1])
         last = source[2]
+        highest = last
         i    = 0
         for entry in feed['entries']:
             i   +=1
             current = int(time.mktime(entry['published_parsed']))
-            zero = int(time.mktime(feed['entries'][0]['published_parsed']))
-            if i==5:
-                self.db.RSSUpdateLastEntry(source[0], zero)
+            if current > highest:
+                highest=current
+            if i==2:
+                self.db.RSSUpdateLastEntry(source[0], highest)
                 break
             elif current != last and current > last:
                 embed = parseEntry(self, entry, last, source,feed['feed'])
-                embeds += [embed]
+                if embed != []:
+                    embeds += embed
             elif current == last:
-                self.db.RSSUpdateLastEntry(source[0], zero)
+                self.db.RSSUpdateLastEntry(source[0], highest)
                 break
+            elif highest == last:
+                print('Uh... nothing new?')
         if embeds != None:
             webhooks = self.db.subbedWebhooks(source[0])
             for sub in webhooks:
-            #    print(sub)
-                await self.endpoints.webhook(embeds, sub[1], sub[0],f"{source[0]}", feed['feed']['image']['url'])
+                av = feed['feed']['image']['url']
+                await self.endpoints.webhook(embeds, sub[1], sub[0],f"{source[0]}", av)
     if embeds != None:
-        return
         webhooks = self.db.subbedWebhooks('all')
         for webhook in webhooks:
-    #        print(webhook)
             if len(embeds) < 11:
                 await self.endpoints.webhook(embeds, webhook[1],webhook[0],'RSS')
             else:
-                for embed in embeds:
-                    await self.endpoints.webhook([embed], webhook[1],webhook[0],'RSS')
+                total = ceil(len(embeds) / 10)
+                for i in range(total):
+                    await self.endpoints.webhook(embeds[:10], webhook[1],webhook[0],'RSS')
+                    del embeds[:10]
                     await asyncio.sleep(1)
             await asyncio.sleep(0.5)
 
@@ -109,18 +114,23 @@ class db():
     def __init__(self):
         pass
     def subbedWebhooks(self, source):
-        return [('','https://discordapp.com/api/webhooks/665572342403170314/77SWAx082_agZMZa47E_3xFp4zwtuBOyHKBbUd53pxP7CGbamJh2aF15qqr0BHIRH_y3')]
+        if source != 'all':
+#            return [('','https://discordapp.com/api/webhooks/665572342403170314/77SWAx082_agZMZa47E_3xFp4zwtuBOyHKBbUd53pxP7CGbamJh2aF15qqr0BHIRH_y3')]
+            return [('','https://discordapp.com/api/webhooks/651726756234919946/rkKk-enkmYxmq6SLzReliFxOIYNLi7RZUkeE9LZBqQtdfEDL16OibBwFmnWe6L85JgXF')]
+        else:
+            return []
     def RSSUpdateLastEntry(self, source, last):
         print('last',last)
     def RSSGetSources(self, language):
-        return [('Dying Light Twitter','https://nitter.net/DyingLightGame/rss',nitter_parsed,'16744744')
-        #,('DyingLightSteam','https://steamcommunity.com/games/239140/rss/',steam_parsed,'500')
+        return [('Dying Light Twitter','https://nitter.net/DyingLightGame/rss',nitter_parsed,'16744744'),
+        ('Dying Light Steam','https://steamcommunity.com/games/239140/rss/?l=english',steam_parsed,'16744744'),
+        ('Dying Light 2 Steam','https://steamcommunity.com/games/534380/rss/?l=english',steam_parsed,'16744744')
         ]
 
 class endpoints():
     def __init__(self):
         pass
-    async def webhook(self, embed,webhook, content, nick, avatar):
+    async def webhook(self, embed,webhook, content, nick, avatar=''):
         r = requests.post(
             webhook, json={"content":content,"embeds":embed, "username":nick,"avatar_url":avatar},
             headers={'Content-Type': 'application/json'}
