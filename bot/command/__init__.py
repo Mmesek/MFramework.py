34832d05953c16065ad7f8ed7d4bde22d703888d
Mmesek <themmesek@gmail.com>
2020-01-13 23:59:31 +0100

    Added recursive import from directory

---
+++ bot/command/__init__.py
@@ -0,0 +1,15 @@
+from os.path import dirname#, basename, isfile, join, isdir
+import glob
+#modules = glob.glob(join(dirname(__file__), "*.py"))
+#__all__ = [ basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]
+sub_modules = glob.glob(f'{dirname(__file__)}/**/*.py',recursive=True)
+fm=[]
+import importlib
+for one in sub_modules:
+    if '__' in one:
+        continue
+    p = dirname(__file__)
+    o = one.replace(f'{p}\\','').replace('\\','.').replace('.py','')
+    importlib.import_module(f'bot.command.{o}')
+    fm+=[o]
+print(fm)
