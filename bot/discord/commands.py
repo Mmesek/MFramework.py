b1463da1dea2b27a4f2560181667398854db5f4b
Mmesek <themmesek@gmail.com>
2020-01-14 20:16:00 +0100

    Partially dehardcoded command's group selection

--- bot/discord/commands.py
+++ bot/discord/commands.py
@@ -1,38 +1,59 @@
 import asyncio, re
+import inspect
 
-from utils import timed
+from bot.utils import timed, parseMention
 
 
-async def Invalid(self, data):
+async def Invalid(*args):
+    #print(f"Invalid Package: {args[0]}")
     return 0
 
+groups = ["System","Admin","Mod","Nitro","Vip","Global"]
+commandList, helpList, extHelpList = {}, {}, {}
 
-commandList = {"Admin":{},"Mod":{},"Nitro":{},"Vip":{},"Global":{}}
-helpList = {"Admin":{},"Mod":{},"Nitro":{},"Vip":{},"Global":{}}
+for i in groups:
+    commandList.update({i:{}})
+    helpList.update({i:{}})
+    extHelpList.update({i:{}})
+
 def register(**kwargs):
-    '''alias="alias"\nhelp="description of help message"\ngroup="Global|Vip|Nitro|Mod|Admin" - Default: Global'''
+    '''alias="alias"\nhelp="description of help message"\ngroup="Global|Vip|Nitro|Mod|Admin" - Default: Global\ncategory="Command's Category" - Default based on Directory'''
     def inner(f, *arg, **kwarg):
-        if 'group' not in kwargs or kwargs['group'] == 'Global':
-            #group = 'Global'
-            group=['Admin','Mod','Nitro','Vip','Global']
-        else:
-            group = kwargs['group']
-        if group == 'Vip':
-            group=['Admin','Mod','Nitro','Vip']
-        elif group == 'Nitro':
-            group=['Admin','Mod','Nitro']
-        elif group == 'Mod':
-            group=['Admin','Mod']
-        elif group == 'Admin':
-            group=['Admin']
+
+        #Set Category
+        if 'category' not in kwargs:
+            #This one should retrieve caller's category based on directory
+            stack = inspect.stack()
+            previous_stack_frame = stack[1]
+            loc = str(previous_stack_frame.filename).split('command\\',1)
+            if len(loc) > 1:
+                category = loc[1].split('\\',1)[0]
+#               group = loc[1].split('\\',1)[1]
+
+        #Set group
+        group = []
+        if 'group' not in kwargs:
+            kwargs['group'] = 'Global'
+        for i in groups:
+            group +=[i]
+            if kwargs['group'] == i:
+                break
+        print(kwargs['group'],f.__name__,group)
+
+        #Register to command's list
         for g in group:
             commandList[g][f"{f.__name__}"] = f
             if 'alias' in kwargs:
                 commandList[group][kwargs['alias']] = f
+
+        #Register help message
         if 'help' in kwargs:
             helpList[group[-1]][f"{f.__name__}"] = kwargs['help']
-        elif f.__doc__ != None:
-            helpList[group[-1]][f"{f.__name__}"] = f.__doc__
+
+        #Register extended help message
+        if f.__doc__ != None:
+            extHelpList[group[-1]][f"{f.__name__}"] = f.__doc__
+
         def inn(*ar, **kwar):
             r = f(*ar, **kwar)
             return r
@@ -48,15 +69,31 @@ seed = {
     5:"Global"
 }
 async def execute(self, data):
-    command = data['content'].split('!',1)[1].split(' ',1)[0]
+    data['content'] = parseMention(data['content'])
+    try: #NICETOHAVE: "text !command args !command2 args2". Currently works: "text !command args" (Although "text !command args text" works only partially. OPTIONAL if anyone cares)
+        command = data['content'].split(f'{self.user_id} ',1)[1]
+        for mention in data['mentions']:
+            if mention['id'] == self.user_id:
+                data['mentions'].remove(mention)
+    except:
+        command = data['content'].split(f'!',1)[1]
+    cmd = command.split(' ',1)
+    try:
+        data['content'] = cmd[1]
+    except:
+        data['content'] = cmd[0] #No content, let's pass command for no reason
+    #.split('!',1)[1].split(' ',1)[0]
     group = self.cache.cachedRoles(data['guild_id'],data['member']['roles'])
     group = seed[group]
-    if await commandList[group].get(command, Invalid)(self, data) == None:
+    if await commandList[group].get(cmd[0], Invalid)(self, data) == None:
         return
     return 0
 
 @register()
 async def help(self, data):
+    #Commands: 
+    #> [Category]: 
+    #[trigger][command] [parameters] - [Description] #(Show only valid for user's group)
     string = 'Commands:'
     for group in helpList:
         #Maybe show only commands for certain group/commands up to certain group?
