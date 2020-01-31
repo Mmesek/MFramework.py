0200477dd2cd9a10a2823dde8ae304e9a8f403c3
Mmesek <13630781+Mmesek@users.noreply.github.com>
2020-01-31 23:18:07 +0100

    Slightly reorganized files. Moved from bot/

--- bot/utils/config.py
+++ bot/utils/config.py
@@ -1,5 +1,6 @@
 import configparser
-path="bot/data/secrets.ini"
+path = "bot/data/secrets.ini"
+
 
 def ConfigToDict():
     dictonary = {}
@@ -16,26 +17,27 @@ def ConfigToDict():
                 value = config.get(section, option)
                 if value.isdigit():
                     value = config.getint(section, option)
-                elif value.lower() in ['true','false','yes','no','on','off'] :
+                elif value.lower() in ['true', 'false', 'yes', 'no', 'on', 'off']:
                     value = config.getboolean(section, option)
-                dictonary[section][option]=value
+                dictonary[section][option] = value
             except Exception as ex:
-                print("Exception while reading from config file: ",ex)
+                print("Exception while reading from config file: ", ex)
                 dictonary[section][option] = None
     return dictonary
 
-    
+
 def GenerateConfig():
     config = configparser.ConfigParser()
     config.read_dict({
-        "Tokens":{'discord':'','steam':'','spotify':'','twitter':'','twitch':''},
-        "Discord":{'presence':'','subscription':False,'presence_type':3},
-        "Spotify":{'client':'','secret':'','auth':''},
-        "Twitter":{'client':'','secret':'','auth':''},
-        "Database":{'location':'raspberry','name':'MBot'}
+        "Tokens": {'discord': '', 'steam': '', 'spotify': '', 'twitter': '', 'twitch': ''},
+        "Discord": {'presence': '', 'subscription': False, 'presence_type': 3},
+        "Spotify": {'client': '', 'secret': '', 'auth': ''},
+        "Twitter": {'client': '', 'secret': '', 'auth': ''},
+        "Database": {'location': 'raspberry', 'name': 'MBot'}
     })
-    with open(path,'w') as file:
+    with open(path, 'w') as file:
         config.write(file)
     print('Generated config file, edit it and restart')
+
 
 cfg = ConfigToDict()
