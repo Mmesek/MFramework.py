d132f5b2ee3f9ef5925263ed2b879286da3a1cc0
Mmesek <13630781+Mmesek@users.noreply.github.com>
2020-01-31 23:39:56 +0100

    Fixed and formatted using Black

--- bot/api.py
+++ bot/api.py
@@ -1,99 +1,128 @@
-import requests, json, operator, time, datetime, asyncio, aiohttp
-from bot.config import cfg as config
+from bot.utils.utils import Embed
+import requests
+import datetime
+import aiohttp
+from bot.utils.config import cfg as config
+
+
 class Spotify:
     def __init__(self):
-        self.rToken  = config['Tokens']['spotify']
-        self.client  = config['Spotify']['client']
-        self.secret  = config['Spotify']['secret']
-        self.auth    = config['Spotify']['auth']
-        self.token   = self.refresh(self.rToken)
-        self.date    = str(datetime.datetime.now())
+        self.rToken = config["Tokens"]["spotify"]
+        self.client = config["Spotify"]["client"]
+        self.secret = config["Spotify"]["secret"]
+        self.auth = config["Spotify"]["auth"]
+        self.token = self.refresh(self.rToken)
+        self.date = str(datetime.datetime.now())
 
     async def spotify_call(self, path, querry, method="GET", **kwargs):
-        defaults = {"headers":{"Authorization": f"Bearer {self.token}", "Accept": "application/json", "Content-Type": "application/json"}}
-        kwargs = dict(defaults,**kwargs)
-        response = await self.ClientSession.request(method, "https://api.spotify.com/v1/"+path+querry, **kwargs)
+        defaults = {
+            "headers": {
+                "Authorization": f"Bearer {self.token}",
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+            }
+        }
+        kwargs = dict(defaults, **kwargs)
+        response = await self.ClientSession.request(method, "https://api.spotify.com/v1/" + path + querry, **kwargs)
         try:
             return await response.json()
         except:
             return
+
     async def connect(self):
         self.ClientSession = aiohttp.ClientSession()
+
     async def disconnect(self):
         await self.ClientSession.close()
+
     def refresh(self, refresh_token):
-        payload = {
-            'grant_type': 'refresh_token',
-            'refresh_token': f'{refresh_token}'
-        }
-        r = requests.post('https://accounts.spotify.com/api/token', data=payload,auth=(f'{self.client}', f'{self.secret}'))
-        return r.json()['access_token']
+        payload = {"grant_type": "refresh_token", "refresh_token": f"{refresh_token}"}
+        r = requests.post(
+            "https://accounts.spotify.com/api/token", data=payload, auth=(f"{self.client}", f"{self.secret}")
+        )
+        return r.json()["access_token"]
 
-    async def new(self, market='gb'):
-        output = {}
-        data = await self.spotify_call('browse/new-releases',f'?country={market}&limit=50&offset=0')
-        for i in data.albums.items:
-            album = self.check(i)
-            output[album['name']] += album
+    async def new(self, market="gb"):
+        output = []
+        data = await self.spotify_call("browse/new-releases", f"?country={market}&limit=50&offset=0")
+        for i in data["albums"]["items"]:
+            album = await self.check(i)
+            if album is not None:
+                output += [album]
+                # output[album['name']] = album
         return output
 
-    async def observed(self, db, market='gb'):
-        output = {}
-        artists = db.GetObservedArtists()
+    async def observed(self, db, market="gb"):
+        output = []
+        artists = await db.GetObservedArtists()
         for one in artists:
-            data = await self.spotify_call(f'artists/{one}/albums',f'?market={market}&limit=50')
-            for i in data.items:
-                album = await self.check(i, one)
-                if album == None:
+            data = await self.spotify_call(f"artists/{one[0]}/albums", f"?market={market}&limit=50")
+            for i in data["items"]:
+                album = await self.check(i, one[1])
+                if album is None:
                     continue
-                elif album['name'] in output:
-                    output[album['name']]['artist'] += (album['artist'])
+                elif album["name"] in output:
+                    # output[album['name']]['artist'] += (album['artist'])
+                    output += [album]
                 else:
-                    output[album['name']] = album
+                    output += [album]
+        #                    output[album['name']] = album
         return output
 
-    async def check(self, chunk, one='Various'):
-        artist = ''
-        if chunk['release_date'][:10] != self.date[:10]:
+    async def check(self, chunk, one="Various"):
+        artist = ""
+        if chunk["release_date"][:10] != self.date[:10]:
             return None
-        for each in chunk['artists']:
-            if each['name'] == 'Various Artists':
-                return None#artist = artist+f"[{artist}]((https://open.spotify.com/artist/{artists[one]})"
+        for each in chunk["artists"]:
+            if each["name"] == "Various Artists":
+                # artist = artist+f"[{artist}]((https://open.spotify.com/artist/{artists[one]})"
+                return None
             else:
-                if artist != '':
-                    artist +=', '
-                artist = artist+f"[{each['name']}]({each['external_urls']['spotify']})"
-        uri     = chunk['uri'].replace('spotify:album:','https://open.spotify.com/album/')
-        name    = f"[{chunk['name']}]({uri})"#{artist}"
-        img     = chunk['images'][0]['url']
-        typ     = chunk['album_type']
-        group   = chunk['album_group']
-        return {"name":name,"img":img,"artist":[(artist)],"type":typ,"group":group}
+                if artist != "":
+                    artist += ", "
+                artist = artist + f"[{each['name']}]({each['external_urls']['spotify']})"
+        uri = chunk["uri"].replace("spotify:album:", "https://open.spotify.com/album/")
+        name = f"[{chunk['name']}]({uri})"  # {artist}"
+        img = chunk["images"][0]["url"]
+        typ = chunk["album_type"]
+        group = chunk["album_type"]
+        return {"name": name, "img": img, "artist": [(artist)], "type": typ, "group": group}
 
-    async def makeList(self, db, market='gb'):
-        result = ''
-        field1,field2 = '',''
-        thumbnail= ''
-        nr = self.new(market)
-        ob = self.observed(db, market)
+    async def makeList(self, db, market="gb"):
+        result = ""
+        field1, field2 = "", ""
+        thumbnail = ""
+        nr = await self.new(market)
+        ob = await self.observed(db, market)
         for item in ob:
-            if thumbnail == '':
-                thumbnail = ob[0]['name']
-            line=f"- {item['name']} - {item['artist']}\n"
-            if len(result)+len(line) < 2024:
-                result+=line
+            if thumbnail == "":
+                thumbnail = ob[0]["img"]
+            line = f"- {item['name']} - {item['artist'][0]}\n"
+            if len(result) + len(line) < 2024:
+                result += line
             elif len(field1) < 1024:
-                field1+=line
+                field1 += line
         for item in nr:
-            field2+=f"- {item['name']} - {item['artist']}"
-        embed = {"description":result,"color":1947988,"author":{"name":f"New Music - {self.date[:10]}",
-        "icon_url":"https://images-eu.ssl-images-amazon.com/images/I/51rttY7a%2B9L.png",
-        "url":"https://open.spotify.com/browse/discover"},"thumbnail":{"url":thumbnail},"footer":{},"fields":[]}
-        embed['fields'] += [{"name":'\u200b','value':field1}]
-        embed['fields'] += [{"name":'\u200b','value':field2}]
+            field2 += f"- {item['name']} - {item['artist'][0]}\n"
+        embed = {
+            "description": result,
+            "color": 1947988,
+            "author": {
+                "name": f"New Music - {self.date[:10]}",
+                "icon_url": "https://images-eu.ssl-images-amazon.com/images/I/51rttY7a%2B9L.png",
+                "url": "https://open.spotify.com/browse/discover",
+            },
+            "thumbnail": {"url": thumbnail},
+            "footer": {},
+            "fields": [],
+        }
+        if field1 != "":
+            embed["fields"] += [{"name": "\u200b", "value": field1}]
+        embed["fields"] += [{"name": "Popular", "value": field2}]
         return embed
 
-'''    async def check1(self, i, one='Various'):
+
+"""    async def check1(self, i, one='Various'):
         for each in i['artists']:
             if len(i['artists']) == 1 and each['name'] != 'Various Artists' and each['name'] != 'Various':
                 #print(len(i['artists']), each['name'])
@@ -112,66 +141,99 @@ class Spotify:
         if date[0:10] == release[0:10]:
             if 'Party' not in i['name'] and'Pop' not in i['name'] and 'Club' not in i['name'] and 'Spice' not in i['name'] and 'Dance' not in i['name'] and 'Mix' not in i['name']:
                 output = output+[([name],[release],[image],[typ], [artist])]#,[typ]]]
-        return output'''
+        return output"""
 
 
 class Steam:
-    #def __init__(self):
-    token = config['Tokens']['steam']
-    steam = 'http://api.steampowered.com/'
-    store = 'https://store.steampowered.com/api/'
-    async def api_call(self, path, querry='', method="GET", api="http://api.steampowered.com/", **kwargs):
+    # def __init__(self):
+    token = config["Tokens"]["steam"]
+    steam = "http://api.steampowered.com/"
+    store = "https://store.steampowered.com/api/"
+
+    async def api_call(self, path, querry="", method="GET", api="http://api.steampowered.com/", **kwargs):
         async with aiohttp.ClientSession() as session:
-            request = await session.request(method, api+path+querry, **kwargs)
-            #print(await request.json())
-            try: 
+            request = await session.request(method, api + path + querry, **kwargs)
+            # print(await request.json())
+            try:
                 return await request.json()
             except:
                 return await request.reason()
-    async def OwnedGames(self,steamid):
-        return await self.api_call("/IPlayerService/GetOwnedGames/v0001/",f"?key={self.token}&steamid={steamid}&format=json")
-    async def PlayerSummaries(self,steamids):
-        return await self.api_call("ISteamUser/GetPlayerSummaries/v2/",f"?key={self.token}&steamid={steamids}")
+
+    async def OwnedGames(self, steamid):
+        return await self.api_call(
+            "/IPlayerService/GetOwnedGames/v0001/", f"?key={self.token}&steamid={steamid}&format=json"
+        )
+
+    async def PlayerSummaries(self, steamids):
+        return await self.api_call("ISteamUser/GetPlayerSummaries/v2/", f"?key={self.token}&steamid={steamids}")
+
     async def getAppList(self):
-        return await self.api_call("IStoreService/GetAppList/v1/",f"?key={self.token}&have_description_language=english")
-    async def News(self,appid):
-        return await self.api_call("ISteamNews/GetNewsForApp/v2/",f"?appid={appid}")
+        return await self.api_call(
+            "IStoreService/GetAppList/v1/", f"?key={self.token}&have_description_language=english"
+        )
+
+    async def News(self, appid):
+        return await self.api_call("ISteamNews/GetNewsForApp/v2/", f"?appid={appid}")
+
     @staticmethod
     async def CurrentPlayers(appid):
-        return await Steam.api_call('ISteamUserStats/GetNumberOfCurrentPlayers/v1/',f'?appid={appid}')
+        return await Steam.api_call("ISteamUserStats/GetNumberOfCurrentPlayers/v1/", f"?appid={appid}")
+
     async def AppList(self):
         return await self.api_call("ISteamApps/GetAppList/v2")
+
     async def Featured(self):
-        return await self.api_call('featured/', api=self.store)
+        return await self.api_call("featured/", api=self.store)
+
     @staticmethod
     async def appDetails(appid):
-        return await Steam.api_call('appdetails/',f'?appids={appid}&l=polish', api=Steam.store)
-    
-from bot.utils import Embed
+        return await Steam.api_call("appdetails/", f"?appids={appid}&l=polish", api=Steam.store)
+
+
 class Twitter:
     def __init__(self):
-        self.token  = config['Tokens']['twitter']
-        self.client = config['Twitter']['Client']
-        self.secret = config['Twitter']['Secret']
-        self.auth   = config['Twitter']['Auth']
-    async def twitter_call(self, path,querry,method="GET", **kwargs):
-        defaults = {"headers":{"Authorization": f"Bearer {self.token}", "Accept": "application/json", "Content-Type": "application/json"}}
-        kwargs = dict(defaults,**kwargs)
-        response = await self.ClientSession.request(method, "https://api.twitter.com/1.1/"+path+'?'+querry, **kwargs)
+        self.token = config["Tokens"]["twitter"]
+        self.client = config["Twitter"]["Client"]
+        self.secret = config["Twitter"]["Secret"]
+        self.auth = config["Twitter"]["Auth"]
+
+    async def twitter_call(self, path, querry, method="GET", **kwargs):
+        defaults = {
+            "headers": {
+                "Authorization": f"Bearer {self.token}",
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+            }
+        }
+        kwargs = dict(defaults, **kwargs)
+        response = await self.ClientSession.request(
+            method, "https://api.twitter.com/1.1/" + path + "?" + querry, **kwargs
+        )
         try:
             return await response.json()
         except:
             return
+
     async def connect(self):
         self.ClientSession = aiohttp.ClientSession()
+
     async def disconnect(self):
         await self.ClientSession.close()
+
     async def user_timeline(self, db, user):
-        tweet = await self.twitter_call('statuses/user_timeline.json',f'screen_name={user}&count=1&include_rts=false')
+        tweet = await self.twitter_call("statuses/user_timeline.json", f"screen_name={user}&count=1&include_rts=false")
         last_tweet_id = db.GetLastTweet(user)
         if tweet[0]["id_str"] != last_tweet_id:
-            embed = Embed().setDescription(tweet[0]["text"]).setAuthor(tweet[0]["user"]["name"],f"https://twitter.com/{tweet[0]['user']['screen_name']}",tweet[0]['user']['profile_image_url'])
-            embed.setColor(int(tweet[0]['user']['profile_background_color'],16)).setFooter('',tweet[0]['created_at'])
-            db.updateLastTweet(user, tweet[0]['id_str'])
+            embed = (
+                Embed()
+                .setDescription(tweet[0]["text"])
+                .setAuthor(
+                    tweet[0]["user"]["name"],
+                    f"https://twitter.com/{tweet[0]['user']['screen_name']}",
+                    tweet[0]["user"]["profile_image_url"],
+                )
+            )
+            embed.setColor(int(tweet[0]["user"]["profile_background_color"], 16)).setFooter("", tweet[0]["created_at"])
+            db.updateLastTweet(user, tweet[0]["id_str"])
             return embed
         return
--- bot/rss.py
+++ bot/rss.py
@@ -1,155 +1,142 @@
-import feedparser, re, requests, asyncio, time, html2text
+import feedparser, re, asyncio, time, html2text
 from bs4 import BeautifulSoup as bs
 from math import ceil
-import bot.utils as utils
-#import utils
+import bot.utils.utils as utils
 
-#TODO: Sort embeds from oldest
+# TODO: Sort embeds from oldest
+
 
 def parseEntry(self, entry, last, source, feed):
-    desc = bs(entry['description'],'html.parser')
-    embed = utils.Embed().setColor(source[3]).setTimestamp(time.strftime("%Y-%m-%dT%H:%M:%S",entry['published_parsed']))
-    if  '//nitter' in source[1]:
-        if entry['author'] == '@DyingLightGame':
-            embed.setUrl(entry['link'].replace('nitter.net','twitter.com')).setTitle(feed['title']).setAuthor(entry['author'],feed['link'].replace('nitter.net','twitter.com'),feed['image']['url']).setDescription(entry['title'])
+    desc = bs(entry["description"], "html.parser")
+    embed = (
+        utils.Embed().setColor(source[3]).setTimestamp(time.strftime("%Y-%m-%dT%H:%M:%S", entry["published_parsed"]))
+    )
+    if "//nitter" in source[1]:
+        if entry["author"] == "@DyingLightGame":
+            embed.setUrl(entry["link"].replace("nitter.net", "twitter.com")).setTitle(feed["title"]).setAuthor(
+                entry["author"], feed["link"].replace("nitter.net", "twitter.com"), feed["image"]["url"]
+            ).setDescription(entry["title"])
         else:
             return []
     else:
-        embed.setTitle(entry['title']).setUrl(entry['link'])
-    if '//steam' in source[1]:
+        embed.setTitle(entry["title"]).setUrl(entry["link"])
+    if "//steam" in source[1]:
         h2t = html2text.HTML2Text()
-        h2tl = h2t.handle(desc.prettify()).replace('\n [','[').replace('\n]',']').replace('[ ','[').replace("{LINK REMOVED}",'').replace('\n\n','\n')
+        h2tl = (
+            h2t.handle(desc.prettify())
+            .replace("\n [", "[")
+            .replace("\n]", "]")
+            .replace("[ ", "[")
+            .replace("{LINK REMOVED}", "")
+            .replace("\n\n", "\n")
+        )
     else:
         h2tl = desc.text
     try:
-        imag = ''
-        if desc.img['src'][-4:] != '.gif':
-            if '//steam' in source[1]:
-                imag = desc.img['src']
+        imag = ""
+        if desc.img["src"][-4:] != ".gif":
+            if "//steam" in source[1]:
+                imag = desc.img["src"]
             else:
-                imag=desc.find('img')['src']
+                imag = desc.find("img")["src"]
     except:
         pass
     try:
-        h2tl = h2tl.replace(f'![]({imag})  \n  \n','')
+        h2tl = h2tl.replace(f"![]({imag})  \n  \n", "")
     except Exception as ex:
         pass
-    links_all = re.findall(r'\((https://store.steam\S*)\)\s',h2tl)
+    links_all = re.findall(r"\((https://store.steam\S*)\)\s", h2tl)
     for link in links_all:
-        s = link.split('/')[-2].replace('_',' ')
-#        print(s,link)
-        h2tl=h2tl.replace(f"[\n","[").replace(f"[{link}]({link})","")
-        embed.addField("Steam Store",f"[{s}]({link})",True)
-    events = re.findall(r'\((\S*/partnerevents/view/\S*)\)\s',h2tl)
+        s = link.split("/")[-2].replace("_", " ")
+        #        print(s,link)
+        h2tl = h2tl.replace(f"[\n", "[").replace(f"[{link}]({link})", "")
+        embed.addField("Steam Store", f"[{s}]({link})", True)
+    events = re.findall(r"\((\S*/partnerevents/view/\S*)\)\s", h2tl)
     for link in events:
-        h2tl=h2tl.replace(f"[\n","[").replace(f"[{link}]({link})","")
-        embed.addField("Steam Event",f"[View Event]({link})",True)
-    images = re.findall(r'\!\[\]\(\S*\)',h2tl)
+        h2tl = h2tl.replace(f"[\n", "[").replace(f"[{link}]({link})", "")
+        embed.addField("Steam Event", f"[View Event]({link})", True)
+    images = re.findall(r"\!\[\]\(\S*\)", h2tl)
     for image in images:
-        h2tl=h2tl.replace(f"{image}",'')
+        h2tl = h2tl.replace(f"{image}", "")
     try:
         desc = h2tl[:2023]
     except Exception as ex:
-        print("RSS Error: ",ex)
-        desc = last['summary_detail']['value']
-    if 'author' in entry:
+        print("RSS Error: ", ex)
+        desc = last["summary_detail"]["value"]
+    if "author" in entry:
         ftext = f"{entry['author']} @ {source[0]}"
     else:
         ftext = source[0]
-    if '//nitter' not in source[1]:
-        embed.setFooter('',ftext).setDescription(desc.replace(' * ','\n').replace('______','-')[:2023])
-    if '//steam' in source[1] or '//nitter' in source[1]:
+    if "//nitter" not in source[1]:
+        embed.setFooter("", ftext).setDescription(desc.replace(" * ", "\n").replace("______", "-")[:2023])
+    if "//steam" in source[1] or "//nitter" in source[1]:
         embed.setImage(imag)
     else:
         embed.setThumbnail(imag)
     return [embed.embed]
 
+
 async def rss(self, language):
-    feeds = self.db.RSSGetSources(language)
+    feeds = await RSSGetSources(self, language)
     for source in feeds:
         embeds = []
         feed = feedparser.parse(source[1])
         last = source[2]
         highest = last
-        i    = 0
-        for entry in feed['entries']:
-            i   +=1
-            current = int(time.mktime(entry['published_parsed']))
+        i = 0
+        for entry in feed["entries"]:
+            i += 1
+            current = int(time.mktime(entry["published_parsed"]))
             if current > highest:
-                highest=current
-            if i==2:
-                self.db.RSSUpdateLastEntry(source[0], highest)
+                highest = current
+            if i == 2:
+                await RSSUpdateLastEntry(self, source[0], highest)
                 break
             elif current != last and current > last:
-                embed = parseEntry(self, entry, last, source,feed['feed'])
+                embed = parseEntry(self, entry, last, source, feed["feed"])
                 if embed != []:
                     embeds += embed
             elif current == last:
-                self.db.RSSUpdateLastEntry(source[0], highest)
+                await RSSUpdateLastEntry(self, source[0], highest)
                 break
             elif highest == last:
-                print('Uh... nothing new?')
-        if embeds != None:
-            webhooks = self.db.subbedWebhooks(source[0])
+                print("Uh... nothing new?")
+        if embeds != []:
+            webhooks = await subbedWebhooks(self, source[0])
             for sub in webhooks:
-                av = feed['feed']['image']['url']
-                await self.endpoints.webhook(embeds, sub[1], sub[0],f"{source[0]}", av)
-    if embeds != None:
-        webhooks = self.db.subbedWebhooks('all')
+                try:
+                    av = feed["feed"]["image"]["url"]
+                except:
+                    av = ""
+                await self.endpoints.webhook(embeds, sub[1], sub[0], f"{source[0]}", av)
+    if embeds is not None:
+        webhooks = await subbedWebhooks(self, "all")
         for webhook in webhooks:
             if len(embeds) < 11:
-                await self.endpoints.webhook(embeds, webhook[1],webhook[0],'RSS')
+                await self.endpoints.webhook(embeds, webhook[1], webhook[0], "RSS")
             else:
                 total = ceil(len(embeds) / 10)
                 for i in range(total):
-                    await self.endpoints.webhook(embeds[:10], webhook[1],webhook[0],'RSS')
+                    await self.endpoints.webhook(embeds[:10], webhook[1], webhook[0], "RSS")
                     del embeds[:10]
                     await asyncio.sleep(1)
             await asyncio.sleep(0.5)
 
-nitter_parsed = 1578412819
-steam_parsed = 0
 
-import requests
-
-class db():
-    def __init__(self):
-        pass
-    def subbedWebhooks(self, source):
-        if source != 'all':
-#            return [('','https://discordapp.com/api/webhooks/665572342403170314/77SWAx082_agZMZa47E_3xFp4zwtuBOyHKBbUd53pxP7CGbamJh2aF15qqr0BHIRH_y3')]
-            return [('','https://discordapp.com/api/webhooks/651726756234919946/rkKk-enkmYxmq6SLzReliFxOIYNLi7RZUkeE9LZBqQtdfEDL16OibBwFmnWe6L85JgXF')]
-        else:
-            return []
-    def RSSUpdateLastEntry(self, source, last):
-        print('last',last)
-    def RSSGetSources(self, language):
-        return [('Dying Light Twitter','https://nitter.net/DyingLightGame/rss',nitter_parsed,'16744744'),
-        ('Dying Light Steam','https://steamcommunity.com/games/239140/rss/?l=english',steam_parsed,'16744744'),
-        ('Dying Light 2 Steam','https://steamcommunity.com/games/534380/rss/?l=english',steam_parsed,'16744744')
-        ]
-
-class endpoints():
-    def __init__(self):
-        pass
-    async def webhook(self, embed,webhook, content, nick, avatar=''):
-        r = requests.post(
-            webhook, json={"content":content,"embeds":embed, "username":nick,"avatar_url":avatar},
-            headers={'Content-Type': 'application/json'}
-        )
-        print("Webhook status code:", r.status_code, r.text)
+async def subbedWebhooks(self, source):
+    return await self.db.selectMultiple("Webhooks", "Webhook, Content", "WHERE Source=?", [source])
 
 
-class Test():
-    def __init__(self):
-        self.db = db()
-        self.endpoints = endpoints()
-    async def run(self):
-        await rss(self, 'pl')
+async def RSSUpdateLastEntry(self, source, last):
+    await self.db.update("RSS", "Last=? WHERE Source=?", [last, source])
 
-if __name__ == "__main__":
-    test = Test()
 
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(test.run())
-    loop.close()
+async def RSSGetSources(self, language):
+    return await self.db.selectMultiple("RSS", "Source, URL, Last, Color", "WHERE Language=?", [language])
+
+
+async def refresh(self, ref):
+    while ref:
+        await rss(self, "pl")
+        await rss(self, "en")
+        asyncio.sleep(900)
