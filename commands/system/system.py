bcf43781ed37e7711699d46d0fe5b8e101e8dc23
Mmesek <themmesek@gmail.com>
2020-04-30 01:07:41 +0200

    Moved import to functions for deferred import

--- commands/system/system.py
+++ commands/system/system.py
@@ -10,8 +10,8 @@ async def updateStatus(self, status="How the World Burns", status_kind="Online",
     """Kind - Online/dnd/idle/invisible/offline\nType - 0 - Playing, 1 - Streaming, 2 - Listening, 3 - Watching"""
     await self.status_update(status_kind, status, status_type, afk)
 
-import importlib
 @register(group="System", help="Reloads commands files.")
 async def reloadCommands(self, *args, data, **kwargs):
     """This probably needs to be placed in another file but well, it's here currently FOR SCIENCE."""
+    import importlib
     importlib.reload(__name__)
