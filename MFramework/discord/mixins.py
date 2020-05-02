a9400ab9d2defc9063be18450bfe2698e8eac259
Mmesek <13630781+Mmesek@users.noreply.github.com>
2020-05-02 09:44:53 +0200

    Added allowed_mentions support

--- MFramework/discord/mixins.py
+++ MFramework/discord/mixins.py
@@ -7,12 +7,12 @@ class EndpointWrappers:
         await self.create_message(channel_id=channel, content=_content, nonce=0, tts=_tts, file=None, embed=_embed, payload_json=None, allowed_mentions=_allowed_mentions, audit_reason='')
     async def sendFile(self, channel, _content, _embed, attachment, filename='', _allowed_mentions={}, _tts=False):
         await self.create_message(channel_id=channel, content=_content, nonce=0, tts=_tts, file=(filename, attachment), embed=_embed, payload_json=None, allowed_mentions=_allowed_mentions, audit_reason='')
-    async def message(self, channel, content):
-        return await self.api_call(f"/channels/{channel}/messages", "POST", json={"content": content})
-    async def embed(self, channel, content, embed):
+    async def message(self, channel, content, allowed_mentions={}):
+        return await self.api_call(f"/channels/{channel}/messages", "POST", json={"content": content, "allowed_mentions":allowed_mentions})
+    async def embed(self, channel, content, embed, allowed_mentions={}):
         if 'color' not in embed:
             embed['color'] = random.randint(0,16777215)#self.color
-        return await self.api_call(f"/channels/{channel}/messages", "POST", json={"content": content, "embed": embed})
+        return await self.api_call(f"/channels/{channel}/messages", "POST", json={"content": content, "embed": embed, "allowed_mentions":allowed_mentions})
     async def webhook(self, embeds, content="", webhook_url="", username="", avatar_url=""):
         return await self.api_call(
             f"/webhooks/{webhook_url}",
