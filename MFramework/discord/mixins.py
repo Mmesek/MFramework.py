078a1cda31fc0ba04ada9b4c34596ec8a3efbce4
Mmesek <themmesek@gmail.com>
2020-05-20 11:55:09 +0200

    Added allowed mentions and moving guild members

--- MFramework/discord/mixins.py
+++ MFramework/discord/mixins.py
@@ -9,18 +9,20 @@ class EndpointWrappers:
         await self.create_message(channel_id=channel, content=_content, nonce=0, tts=_tts, file=(filename, attachment), embed=_embed, payload_json=None, allowed_mentions=_allowed_mentions, audit_reason='')
     async def message(self, channel, content, allowed_mentions={}):
         return await self.api_call(f"/channels/{channel}/messages", "POST", json={"content": content, "allowed_mentions":allowed_mentions})
-    async def embed(self, channel, content, embed, allowed_mentions={}):
-        if 'color' not in embed:
+    async def embed(self, channel, content, embed={}, allowed_mentions={}):
+        if embed != {} and 'color' not in embed:
             embed['color'] = random.randint(0,16777215)#self.color
         return await self.api_call(f"/channels/{channel}/messages", "POST", json={"content": content, "embed": embed, "allowed_mentions":allowed_mentions})
-    async def webhook(self, embeds, content="", webhook_url="", username="", avatar_url=""):
+    async def webhook(self, embeds, content="", webhook_url="", username="", avatar_url="", allowed_mentions={}):
         return await self.api_call(
             f"/webhooks/{webhook_url}",
             "POST",
-            json={"content": content, "embeds": embeds, "username": username, "avatar_url": avatar_url})
+            json={"content": content, "embeds": embeds, "username": username, "avatar_url": avatar_url, "allowed_mentions":allowed_mentions})
     async def withFile(self, channel, attachment, filename="", content="", embed=""):
         return await self.api_call(
             f"/channels/{channel}/messages",
             "POST",
             json={"content": content, "embed": embed},
             file=(filename, attachment))
+    async def move_guild_member(self, guild_id, user_id, channel_id, audit_reason='') -> None:
+        return await self.api_call(f'/guilds/{guild_id}/members/{user_id}', 'PATCH', reason=audit_reason, json={'channel_id':channel_id})
