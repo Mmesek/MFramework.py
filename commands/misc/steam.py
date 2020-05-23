ca07836b30435beababc1d0f1322f02d8c06adf4
Mmesek <13630781+Mmesek@users.noreply.github.com>
2020-05-24 00:19:33 +0200

    Added localization to SteamAPI commands

--- commands/misc/steam.py
+++ commands/misc/steam.py
@@ -3,7 +3,7 @@ import json
 
 from MFramework.commands import register
 from MFramework.utils.api import Steam as steam
-from MFramework.utils.utils import Embed
+from MFramework.utils.utils import Embed, tr
 
 
 @register(group="System", help="Updates Index of Steam Games")
@@ -22,7 +22,7 @@ async def loadSteamIndex(self):
         self.index = json.load(fjson)
 
 
-async def steamParse(self, request, *game):
+async def steamParse(self, request, language, *game):
     from difflib import get_close_matches
     game = " ".join(game)
     games = game.split(",")
@@ -35,62 +35,62 @@ async def steamParse(self, request, *game):
             playercount = await steam.CurrentPlayers(appid)
             yield playercount, game
         elif request == "details":
-            page = await steam.appDetails(appid)
+            page = await steam.appDetails(appid, language)
             yield page[str(appid)]["data"], appid
 
 
 @register(help="Fetches playercount for specified game(s). Separate with `,`", alias="", category="")
-async def playercount(self, *game, data, **kwargs):
-    result = "Playercount for "
-    async for playercount, game in steamParse(self, "playercount", *game):
+async def playercount(self, *game, data, language, **kwargs):
+    result = tr("commands.playercount.for", language)
+    async for playercount, game in steamParse(self, "playercount", language, *game):
         try:
             playercount = playercount["response"]["player_count"]
             result += f"{game}: {playercount}\n"
         except KeyError:
-            result += f"{game}: Not found: Game might be not available, released or an error occured\n"
+            result += f"{game}: "+tr("commands.playercount.error", language)
     await self.message(data.channel_id, result[:2000])
 
 
 
 
 @register(help="Shows game details", alias="", category="")
-async def gameDetails(self, *game, data, **kwargs):
-    async for game, appid in steamParse(self, "details", *game):
+async def game(self, *game, data, language, **kwargs):
+    async for game, appid in steamParse(self, "details", language, *game):
         embed = Embed()
         embed.setDescription(game["short_description"]).setTitle(game["name"])
         embed.setUrl(f"https://store.steampowered.com/app/{appid}/").setFooter(
-            "", text="Release Date: " + game["release_date"]["date"]
+            "", text=tr("commands.game.release", language) + game["release_date"]["date"]
         )
         embed.setImage(game["header_image"])
         prc = game.get("price_overview", {}).get("final_formatted")
         is_free = game.get("is_free", {})
         if prc is not None or is_free:
             if is_free:
-                prc = "Free2Play"
-            embed.addField("Price", prc, True)
+                prc = tr("commands.game.f2p", language)
+            embed.addField(tr("commands.game.price", language), prc, True)
         r = game.get("recommendations", {}).get("total")
         if r is not None:
-            embed.addField("Recommendations", r, True)
+            embed.addField(tr("commands.game.recommendations", language), r, True)
         cp = await steam.CurrentPlayers(appid)
         cp = cp.get("response", {}).get("player_count")
         if cp is not None:
-            embed.addField("Currently players", cp, True)
+            embed.addField(tr("commands.game.players", language), cp, True)
         ach = game.get("achievements", {}).get("total")
         if ach is not None and ach != 0:
-            embed.addField("Achievements", ach, True)
+            embed.addField(tr("commands.game.achievements", language), ach, True)
         required_age = game.get("required_age")
         if required_age != 0:
-            embed.addField("Required Age", required_age, True)
+            embed.addField(tr("commands.game.age", language), required_age, True)
         dlc = len(game.get("dlc", []))
         if dlc != 0:
-            embed.addField("DLC Count", dlc, True)
+            embed.addField(tr("commands.game.dlc", language), dlc, True)
         f = len(embed.embed["fields"])
         if f != 0 and f % 3 != 0:
             embed.addField("\u200b", "\u200b", True)
         devs = game.get("developers")
         if devs is not None:
-            embed.addField("Developers", ", ".join(devs), True)
+            embed.addField(tr("commands.game.developers", language, count=len(devs)), ", ".join(devs), True)
         publishers = game.get("publishers")
         if publishers != devs:
-            embed.addField("Publishers", ", ".join(publishers), True)
+            embed.addField(tr("commands.game.publishers", language, count=len(publishers)), ", ".join(publishers), True)
         await self.embed(data.channel_id, "", embed.embed)
