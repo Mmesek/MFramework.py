f6521d0c5a84627930427eeff83fa8b65df0ad02
Mmesek <13630781+Mmesek@users.noreply.github.com>
2020-05-24 18:42:14 +0200

    Cleaned webhook adding commands

--- commands/system/webhooks.py
+++ commands/system/webhooks.py
@@ -1,13 +1,5 @@
 from MFramework.commands import register
-from MFramework.database.alchemy import RSS, Webhooks, Spotify
-
-
-@register(group="System", help="Add RSS to watchlist")
-async def add_rss(self, name, url, language, color, avatar_url=None, *args, data, **kwargs):
-    s = self.db.sql.session()
-    r = RSS(name, 0, url, color, language, avatar_url)
-    s.add(r)
-    s.commit()
+from MFramework.database.alchemy import Webhooks
 
 
 @register(group="System", help="Sub current channel to a RSS source")
@@ -22,10 +14,10 @@ async def sub_rss(self, name, webhook="", content="", regex="", *args, channel, 
                 webhook = f"{wh.id}/{wh.token}"
                 break
     if webhook == "":
-        if "dm" not in data.content.casefold():
-            name = "RSS"
-        elif "log" in data.content.casefold():
+        if "log" in data.content.casefold():
             name = "Logging"
+        elif "dm" not in data.content.casefold():
+            name = "RSS"
         else:
             name = "DM Inbox"
         wh = await self.create_webhook(channel, name, f"Requested by {data.author.username}")
@@ -34,49 +26,3 @@ async def sub_rss(self, name, webhook="", content="", regex="", *args, channel, 
     w = Webhooks(data.guild_id, webhook, name, content, regex, data.author.id)
     s.add(w)
     s.commit()
-
-
-@register(group="System", help="Add Artist to tracking new releases")
-async def addSpotify(self, spotifyID, artist, *args, data, **kwargs):
-    s = self.db.sql.session()
-    spotify = Spotify(spotifyID, artist, data.author.id)
-    s.add(spotify)
-    s.commit()
-
-
-from MFramework.utils.api import Spotify as sptfy
-
-
-@register(group="System", help="Fetch new spotify releases")
-async def spotify(self, *args, data, webhook=False, **kwargs):
-    s = sptfy()
-    await s.connect()
-    try:
-        embed = await s.makeList(db=self.db)
-    finally:
-        await s.disconnect()
-    if not webhook:
-        await self.embed(data.channel_id, "", embed)
-    elif webhook:
-        s = self.db.sql.session()
-        w = s.query(Webhooks).filter(Webhooks.Source == "Spotify").all()
-        for one in w:
-            await self.webhook(
-                [embed],
-                one.Content,
-                one.Webhook,
-                "Spotify",
-                "https://images-eu.ssl-images-amazon.com/images/I/51rttY7a%2B9L.png",
-            )
-
-
-from MFramework.utils.rss import rss
-
-
-@register(group="System", help="Fetch RSS manually")
-async def fetch_rss(self, *args, data, **kwargs):
-    try:
-        await rss(self, "pl")
-        await rss(self, "en")
-    except:
-        await rss(self, "en")
