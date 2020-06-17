5b27e7aace650091af8b0577ad7c235d93ace1e0
Mmesek <13630781+Mmesek@users.noreply.github.com>
2020-06-17 15:37:25 +0200

    Updated paths in mbot.sh

--- mbot.sh
+++ mbot.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 cd /home/pi/MFramework.py/
-source /home/pi/MFramework.py/MFramework/bin/activate
-/usr/bin/screen -dm -S "MFramework" /home/pi/MFramework.py/MFramework/bin/python3.8 /home/pi/MFramework/__main__.py
+/usr/bin/screen -dm -S "MFramework" /usr/bin/python3.7 /home/pi/MFramework.py/__main__.py
