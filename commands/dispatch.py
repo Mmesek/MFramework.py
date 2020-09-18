9b0a310d911c609118277699ff4463ed04f8a4ea
Mmesek <13630781+Mmesek@users.noreply.github.com>
2020-09-18 23:56:47 +0200

    Fixed not returning from voice_state_update if bot

--- commands/dispatch.py
+++ commands/dispatch.py
@@ -333,7 +333,7 @@ async def presence_update(self, data):
 async def voice_state_update(self, data):
     from MFramework.utils.voice_channels import _check_afk_channel, _check_if_bot, _handle_dynamic_channel, _handle_voice_activity, _handle_voice_link
     if data.member.user.bot:
-        _check_if_bot(self, data)
+        return _check_if_bot(self, data)
     if data.channel_id in self.cache[data.guild_id].disabled_channels and not any(r in data.member.roles for r in self.cache[data.guild_id].disabled_roles):
         data = _check_afk_channel(self, data)
     if self.cache[data.guild_id].dynamic_channels and data.channel_id in self.cache[data.guild_id].dynamic_channels:
