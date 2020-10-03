461507778c15e6f807cae6541b4586efa718d6c3
Mmesek <13630781+Mmesek@users.noreply.github.com>
2020-10-03 18:33:34 +0200

    Updated and improved list_emoji command

--- commands/moderation/admin.py
+++ commands/moderation/admin.py
@@ -27,16 +27,26 @@ async def aemoji(self, emoji_name, *args, data, **kwargs):
     await self.message(data.channel_id, message)
 
 
-@register(group="Admin", help="Lists all available emoji's in guild", notImpl=True)
-async def list_emoji(self, *args, data, **kwargs):
-    emojis = await self.list_guild_emoji(data.guild_id)
-    elist = ""
+@register(group="Admin", help="Lists all available emoji's in guild")
+async def list_emoji(self, *args, data, all=False, regular=False, **kwargs):
+    emojis = await self.list_guild_emojis(data.guild_id)
+    _animated = ""
+    _regular = ""
     for emoji in emojis:
-        if emoji.animated:
-            elist += f"\n> <a:{emoji.name}:{emoji.id}> - \\<a:{emoji.name}:{emoji.id}>"
-        elif "all" in data.content:
-            elist += f"\n> <:{emoji.name}:{emoji.id}> - \\<:{emoji.name}:{emoji.id}>"
-    await self.message(data.channel_id, elist[:2000])
+        if emoji.animated and not regular:
+            _animated += f"\n<a:{emoji.name}:{emoji.id}> - a:{emoji.name}:{emoji.id}"
+        elif not emoji.animated and (all or regular):
+            _regular += f"\n<:{emoji.name}:{emoji.id}> - {emoji.name}:{emoji.id}"
+    e = Embed().setTitle("Emoji List")
+    if not regular and _animated != "":
+        e.addFields("Animated", _animated, False)
+    if (all or regular) and _regular != "":
+        if regular and (len(_regular) / 1024 <= 2):
+            inline = True
+        else:
+            inline = False
+        e.addFields("Regular", _regular, inline)
+    await self.embed(data.channel_id, '', e.embed)
 
 
 @register(group="Admin", help="Delete's message", notImpl=True)
