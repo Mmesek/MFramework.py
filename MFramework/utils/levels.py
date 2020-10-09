227fdee42912ec984a221901fb67b4ede5f6a169
Mmesek <13630781+Mmesek@users.noreply.github.com>
2020-10-09 20:44:50 +0200

    Fixed handling level roles&cleaned ordering them

--- MFramework/utils/levels.py
+++ MFramework/utils/levels.py
@@ -1,17 +1,11 @@
 async def handle_roles(self, data, l, levelRole, x, user_id):
     await self.add_guild_member_role(data.guild_id, user_id, levelRole.Role, "EXP Role")
-    if len(l) > 1:
-        if levelRole.Type == 'AND':
-            #print("AND Level Up!")
-            previous_role = sorted(l, key=lambda i: i.ReqEXP, reverse=True)[x - 1]
-        elif levelRole.Type == 'OR':
-            #print("OR Level Up!")
-            previous_role = sorted(l, key=lambda i: i.ReqEXP, reverse=True)[x - 1]
-        else:
-            #print("COMBINED Level Up!")
-            previous_role = sorted(l, key=lambda i: i.ReqEXP + i.ReqVEXP, reverse=True)[x - 1]                
-        if not previous_role.Stacked and previous_role.Role in data.member.roles:
-            await self.remove_guild_member_role(data.guild_id, user_id, previous_role.Role, "EXP Role")
+    try:
+        previous_role = l[x+1]
+    except:
+        return
+    if not previous_role.Stacked and previous_role.Role in data.member.roles:
+        await self.remove_guild_member_role(data.guild_id, user_id, previous_role.Role, "EXP Role")
 
 async def handle_exp(self, data, e=None):
     if e is None:
@@ -24,14 +18,16 @@ async def handle_exp(self, data, e=None):
     l = self.cache[data.guild_id].levels
     if l == []:
         return
-    for x, levelRole in enumerate(reversed(l)):
-        if levelRole.Role not in data.member.roles and (((levelRole.ReqEXP <= e.EXP) or (levelRole.ReqVEXP <= e.vEXP)) or (levelRole.ReqEXP+levelRole.ReqVEXP <= e.EXP+e.vEXP)):
+    for x, levelRole in enumerate(l):
+        if levelRole.Role not in data.member.roles:
             if levelRole.Type == 'AND' and (e.EXP >= levelRole.ReqEXP and e.vEXP >= levelRole.ReqVEXP):
                 await handle_roles(self, data, l, levelRole, x, user_id)
             elif levelRole.Type == 'OR' and (e.EXP >= levelRole.ReqEXP or e.vEXP >= levelRole.ReqVEXP):
                 await handle_roles(self, data, l, levelRole, x, user_id)
-            elif levelRole.Type == 'COMBINED' and (e.EXP + e.vEXP) >= (levelRole.ReqEXP + levelRole.ReqVEXP):
+            elif levelRole.Type == 'COMBINED' and ((e.EXP + e.vEXP) >= (levelRole.ReqEXP + levelRole.ReqVEXP)):
                 await handle_roles(self, data, l, levelRole, x, user_id)
             else:
                 continue
+            break
+        else:
             break
