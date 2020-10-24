37e8e10cad6b6652636f6837b0830e7f9e80f44d
Mmesek <13630781+Mmesek@users.noreply.github.com>
2020-10-24 23:41:20 +0200

    Added hhelp command and additional help messages

--- commands/halloween2020/halloween.py
+++ commands/halloween2020/halloween.py
@@ -1,7 +1,6 @@
 from MFramework.database import alchemy as db
-
+from MFramework.utils.utils import Embed
 from .helpers import *
-
 @HalloweenEvent(group="System")
 async def halloween_createRoles(self, *args, data, language, **kwargs):
     '''Extended description to use with detailed help command'''
@@ -19,3 +18,26 @@ async def halloween_addRoles(self, *args, data, language, **kwargs):
     users = s.query(db.HalloweenClasses).filter(db.HalloweenClasses.GuildID == data.guild_id).all()
     for user in users:
         await self.add_guild_member_role(data.guild_id, user.UserID, roles.get(user.CurrentClass, ""), "Halloween Minigame")
+
+@HalloweenEvent(help="Shows Halloween commands help")
+async def hhelp(self, *args, data, language, **kwargs):
+    from .helpers.constants import _HHELP
+    s = self.db.sql.session()
+    user = get_user(data.guild_id, data.author.id, s)
+    if user.CurrentClass in MONSTERS:
+        _race = 'Monsters'
+    elif user.CurrentClass in HUNTERS:
+        _race = 'Hunters'
+    else:
+        _race = "Humans"
+    e = Embed()
+    s = {}
+    for race in _HHELP:
+        for h in _HHELP[race]:
+            if race not in s:
+                s[race] = ''
+            s[race] += f'\n`{h}` - ' + _HHELP[race][h]
+    for race in s:
+        e.addField(race if race != 'None' else 'Global', s[race])
+    await self.embed(data.channel_id, '', e.embed)
+    
