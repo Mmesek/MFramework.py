d3c35ccd8102834375f31cefef1a19f15a5b98d3
Mmesek <themmesek@gmail.com>
2020-10-24 23:18:09 +0200

    Updated code to use wrapper translation func&Added missing translations

--- commands/halloween2020/hunters.py
+++ commands/halloween2020/hunters.py
@@ -1,5 +1,4 @@
 from MFramework.database import alchemy as db
-from MFramework.utils.utils import tr
 
 from datetime import timedelta
 from random import SystemRandom
@@ -18,15 +17,15 @@ async def defend(self, *user, data, language, **kwargs):
     if self_user.ActionCooldownEnd is not None:
         if now < self_user.ActionCooldownEnd:
             cooldown = self_user.ActionCooldownEnd - now
-            return await self.message(data.channel_id, tr("events.halloween.remainingCooldown", language, cooldown=cooldown))
+            return await self.message(data.channel_id, _t("remainingCooldown", language, cooldown=cooldown))
 
     target = get_user_id(user)
     if target == data.author.id:
-        return await self.message(data.channel_id, tr("events.halloween.failed_defend", language))
+        return await self.message(data.channel_id, _t("failed_defend", language))
     target_user = s.query(db.HalloweenClasses).filter(db.HalloweenClasses.GuildID == data.guild_id, db.HalloweenClasses.UserID == target).first()
     
     if target_user.CurrentClass not in HUNTERS:
-        return await self.message(data.channel_id, tr("events.halloween.cant_defend", language))
+        return await self.message(data.channel_id, _t("cant_defend", language))
     
     if target_user.ProtectionEnds is None or target_user.ProtectionEnds < now:
         duration = SystemRandom().randint(5, 40)
@@ -36,9 +35,9 @@ async def defend(self, *user, data, language, **kwargs):
         self_user.ActionCooldownEnd = now + timedelta(hours=1)
         s.add(db.HalloweenLog(data.guild_id, target_user.UserID, target_user.CurrentClass, target_user.CurrentClass, self_user.UserID, now))
         s.commit()
-        return await self.message(data.channel_id, tr("events.halloween.success_defend", language, duration=duration))
+        return await self.message(data.channel_id, _t("success_defend", language, duration=duration))
 
-    return await self.message(data.channel_id, tr("events.halloween.error_defend", language))
+    return await self.message(data.channel_id, _t("error_defend", language))
 
 @Hunters()
 async def betray(self, *user, data, language, **kwargs):
@@ -50,11 +49,11 @@ async def betray(self, *user, data, language, **kwargs):
     now = get_current_time()
     if self_user.ActionCooldownEnd is not None and now < self_user.ActionCooldownEnd:
         cooldown = self_user.ActionCooldownEnd - now
-        return await self.message(data.channel_id, tr("events.halloween.remainingCooldown", language, cooldown=cooldown))
+        return await self.message(data.channel_id, _t("remainingCooldown", language, cooldown=cooldown))
     roll = SystemRandom().randint(0, 100)
     self_user.ActionCooldownEnd = now + timedelta(hours=4)
     s.commit()
     if roll > 97:
         await turning_logic(self, data, user, HUNTERS, True, skip_cooldown=True)
-        return await self.message(data.channel_id, tr("events.halloween.success_betray", language))
-    return await self.message(data.channel_id, tr("events.halloween.error_betray", language))
+        return await self.message(data.channel_id, _t("success_betray", language))
+    return await self.message(data.channel_id, _t("error_betray", language))
