0737232a65bf574beca14c9f648b42e00a4e6bff
Mmesek <13630781+Mmesek@users.noreply.github.com>
2020-10-29 20:07:18 +0100

    Added alternative dice reactions

--- commands/rpg/dice.py
+++ commands/rpg/dice.py
@@ -16,7 +16,13 @@ async def roll(self, data, update=False):
             1:'1️⃣',2:'2️⃣',3:'3️⃣',
             4:'4️⃣',5:'5️⃣',6:'6️⃣'
         }
+        dices = {
+            0:'dice_0:761760091648294942',
+            1:'dice_1:761760091971780628',2:'dice_2:761760091837825075',3:'dice_3:761760092206792750',
+            4:'dice_4:761760092767911967',5:'dice_5:761760093435068446',6:'dice_6:761760093817143345'
+        }
         z = re.findall(r"(?i)zabij|wyryw|mord", reg[0])
         if z:
             v = 0
-        await self.create_reaction(data.channel_id, data.id, reactions.get(v))
+        r = reactions.get(v) if self.user_id != 372187189087567872 else dices.get(v)
+        await self.create_reaction(data.channel_id, data.id, r)
