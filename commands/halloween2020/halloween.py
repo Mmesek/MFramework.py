1b0508038bb1d5dbd3b67b09054486ac514e39a4
Mmesek <13630781+Mmesek@users.noreply.github.com>
2020-11-04 22:58:45 +0100

    Added hsummary command as a summary for halloween event

--- commands/halloween2020/halloween.py
+++ commands/halloween2020/halloween.py
@@ -57,4 +57,72 @@ async def hhelp(self, *args, data, language, **kwargs):
         r = _t('race_'+race.lower(), language)
         e.addField(r, s[race])
     await self.embed(data.channel_id, '', e.embed)
-    
+
+@register(group='System', help='Shows Halloween 2020 statistics graph', alias='', category='')
+async def hsummary(self, *args, data, resample='D', locator='Day', interval=1, growth=False, language, **kwargs):
+    '''Extended description to use with detailed help command'''
+    from datetime import date, datetime, timezone
+    import MFramework.utils.graphing as graph
+    await self.trigger_typing_indicator(data.channel_id)
+
+    _s = self.db.sql.session()
+    history = _s.query(db.HalloweenLog).filter(db.HalloweenLog.GuildID == data.guild_id).order_by(db.HalloweenLog.Timestamp.asc()).all()
+    _total = _s.query(db.HalloweenClasses).filter(db.HalloweenClasses.GuildID == data.guild_id).order_by(db.HalloweenClasses.TurnCount.desc()).all()
+    e = Embed().setTitle("Thanks for participating!")
+    totalBites, totalPopulation = get_total(self, data, _total)
+    totalPopulation = sorted(totalPopulation.items(), key=lambda i: i[1], reverse=True)
+    totalPopulation = {i[0]: i[1] for i in totalPopulation}
+    e.setDescription(_t("total_bites", language) + '/' + _t("total_cures", language) + f": {totalBites}\n" + '\n'.join('{}: {}'.format(_t(i.lower().replace(' ', '_'), language, count=totalPopulation[i]).title(), totalPopulation[i]) for i in totalPopulation))
+    total = {}
+    _counts = {}
+    _first = {}
+    y = []
+    x = []
+    for entry in history:
+        if entry.ToClass not in total:
+            total[entry.ToClass] = {'x': [], 'y': []}
+        if entry.FromClass not in total:
+            total[entry.FromClass] = {'x':[],'y':[]}
+        if entry.ToClass not in _counts:
+            _counts[entry.ToClass] = 0
+        if entry.FromClass not in _counts:
+            _counts[entry.FromClass] = 0
+
+        _counts[entry.FromClass] -= 1
+        if entry.FromClass not in _first:
+            _first[entry.FromClass] = False
+        _counts[entry.ToClass] += 1
+        if entry.ToClass not in _first:
+            _first[entry.ToClass] = True
+        
+        total[entry.FromClass]['x'].append(entry.Timestamp)
+        total[entry.FromClass]['y'].append(_counts[entry.FromClass])
+        total[entry.ToClass]['x'].append(entry.Timestamp)
+        total[entry.ToClass]['y'].append(_counts[entry.ToClass])
+
+    for i in total:
+        if total[i] == {}:
+            continue
+        m = min(total[i]['y'])
+        if m < 0:
+            n_list = []
+            for j in total[i]['y']:
+                n_list.append(j + abs(m))
+            total[i]['y'] = n_list
+        d = totalPopulation[i] - total[i]['y'][-1]
+        if total[i]['y'][-1] < totalPopulation[i]:
+            n_list = []
+            for j in total[i]['y']:
+                n_list.append(j + d)
+            total[i]['y'] = [d] + n_list
+            total[i]['x'] = [datetime(2020, 10, 17, tzinfo=timezone.utc)] + total[i]['x']
+    for i in total:
+        if total[i] != {}:
+            total[i]['y'] = [0] + total[i]['y']
+            total[i]['x'] = [datetime(2020, 10, 16, tzinfo=timezone.utc)] + total[i]['x']
+    total['Human'] = {}
+
+    img_str = graph.plot(total, resample, growth, locator, interval)
+    file_name = f"growth-{date.today()}.png"
+    e.setImage("attachment://"+file_name).setColor(16744206)
+    await self.withFile(data.channel_id, img_str, file_name, "", e.embed)
