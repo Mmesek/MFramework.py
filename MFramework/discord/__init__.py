3867c3b20b5b8bbd6cad1a5f7fa9be05a1d5c4f0
Mmesek <13630781+Mmesek@users.noreply.github.com>
2021-02-17 12:23:21 +0100

    Added deserializer that casts dict to an object

--- MFramework/discord/__init__.py
+++ MFramework/discord/__init__.py
@@ -24,3 +24,23 @@ def onDispatch(f):
         Dispatch[f.__name__.upper()].append(f)
         return f
     return inner(f)
+
+class Deserializer:
+    def __init__(self):
+        import zlib
+        self._buffer = bytearray()
+        self._zlib = zlib.decompressobj()
+    def __call__(self, msg: bytes):
+        import ujson as json
+        if type(msg) is bytes:
+            self._buffer.extend(msg)
+            if len(msg) >= 4:
+                if msg[-4:] == b'\x00\x00\xff\xff':
+                    msg = self._zlib.decompress(self._buffer).decode('utf-8')
+                    self._buffer = bytearray()
+                else:
+                    return
+            else:
+                return
+        from .objects import Gateway_Payload
+        return Gateway_Payload(**json.loads(msg)) #TODO: Cast to object, not just json here!
