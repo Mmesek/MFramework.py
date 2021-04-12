daa71b531782ccbdd1d611a4a082044879080f86
Mmesek <13630781+Mmesek@users.noreply.github.com>
2021-04-12 22:37:43 +0200

    Added Bot specific constructor

--- MFramework/__init__.py
+++ MFramework/__init__.py
@@ -28,8 +28,6 @@ ver_date = time.strftime("%Y-%m-%dT%H:%M:%S", time.gmtime(commits[-1].time[0]))
 
 
 from mdiscord import *
-class Bot(Client):
-    pass
 from .database.database import Database
 from .database.cache import Cache
 from .utils.utils import log # noqa: F401
@@ -37,11 +35,22 @@ from .utils.utils import log # noqa: F401
 from typing import Dict
 
 class Bot(Client):
-    user_id: Snowflake = None
     session_id: str = None
     start_time: float = None
+    
+    alias: str = "?"
+    emoji: dict = dict
+    primary_guild: Snowflake = 463433273620824104
 
     db: Database
     cache: Dict[Snowflake, Cache]
+    def __init__(self, name: str, cfg: dict, db: Database=None, cache: Cache=None, shard: int=0, total_shards: int=1):
+        self.db = db
+        self.cache = cache
+        self.alias = cfg[name].get('alias', '?')
+        self.emoji = cfg["Emoji"]
+        self.primary_guild = cfg[name].get('primary_guild', 463433273620824104)
+
+        super().__init__(name, cfg, shard=shard, total_shards=total_shards)
 
 from .commands.interactions import register, Groups, Event # noqa: F401
