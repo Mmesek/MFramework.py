0f083463e22b7904ae70539d659fdc2a0b2638d7
Mmesek <13630781+Mmesek@users.noreply.github.com>
2021-04-25 18:01:21 +0200

    Cleanup & Merged dice.roll directly into function

--- dispatch/actions.py
+++ dispatch/actions.py
@@ -1,99 +1,55 @@
 from MFramework import *
 from MFramework.database.alchemy import types
-async def _randomize(ctx: Bot, data: Message, chance: int, reaction: str, name: str, _type: types.Item=None, 
+async def _handle_reaction(ctx: Bot, data: Message, reaction: str, name: str, _type: types.Item=None, 
                     wait: bool=True, delete_own: bool=True, store_in_cache: bool=False, first_only=False, all_reactions: bool=True, 
                     logger: str=None, statistic: types.Statistic=None):
-    import random
-    if random.SystemRandom().randint(1, 100) < chance:
-        import asyncio
-        await asyncio.sleep(random.SystemRandom().randint(0, 10))
-        await data.react(reaction)
-        t = random.SystemRandom().randint(15, 60)
-        if store_in_cache:
-            ctx.cache[data.guild_id].special_message.store(data, expire=t, type=name, first_only=first_only)
-        if not wait and not statistic:
-            return
-        await asyncio.sleep(t)
-        if delete_own:
-            await data.delete_reaction(reaction)
-        
-        if store_in_cache:
-            ctx.cache[data.guild_id].special_message.delete(data.id) #Not needed thanks to expire flag
-
-        s = ctx.db.sql.Session()
-
-        from MFramework.database.alchemy import models
-        if statistic:
-            models.Statistic.increment(s, data.guild_id, statistic)
-        if all_reactions:
-            from MFramework.utils.utils import get_all_reactions
-            users = await get_all_reactions(ctx, data.channel_id, data.id, reaction)
-        
-            from MFramework.database.alchemy import items, helpers
-            item = helpers.fetch_or_add(s, items.Item, name, _type)
-            i = items.Inventory(item)
-            for user in users:
-                helpers.add_item(s, data.guild_id, user.id, item=i)
-                models.Log.claim(data.guild_id, user.id, _type)
-            await ctx.cache[data.guild_id].logging[logger](data, users)
-        s.commit()
-
-from MFramework.commands.interactions import Event
-@Event(month=4)
-async def egg_hunt(ctx: Bot, data: Message):
-    # V RNG
-    # V React
-    # Cache Message ID
-    # Check if reaction is for cached Message ID and add to cache/db
-    # After elapsed, remove reactions from message (or remove them instantly)
-    # Log score?
-    from MFramework.database.alchemy import types
-    await _randomize(ctx, data, 2, "🥚", "Easter Egg", types.Item.EasterEgg, logger="egg_hunt", statistic=types.Statistic.Spawned_Eggs)
-    return
-    import random
-    if random.SystemRandom().randint(1, 100) < 5:
-        import asyncio
-        await asyncio.sleep(random.SystemRandom().randint(0, 10))
-        await data.react("🥚")
-        t = random.SystemRandom().randint(15, 60)
-        #ctx.cache[data.guild_id].special_message.store(data, expire=t, type="Easter Egg")
-        await asyncio.sleep(t)
-        
-        from MFramework.utils.utils import get_all_reactions
-        users = await get_all_reactions(ctx, data.channel_id, data.id, "🥚")
-        #ctx.cache[data.guild_id].special_message.delete(data.id)
-        await data.delete_reaction("🥚")
-
-        from MFramework.database.alchemy import items, types, helpers, models
-        s = ctx.db.sql.Session()
-        models.Statistic.increment(s, data.guild_id, types.Statistic.Spawned_Eggs)
-
-        item = helpers.fetch_or_add(s, items.Item, "Easter Egg", types.Item.EasterEgg)
+    import random, asyncio
+    await asyncio.sleep(random.SystemRandom().randint(0, 10))
+    await data.react(reaction)
+    t = random.SystemRandom().randint(15, 60)
+    if store_in_cache:
+        ctx.cache[data.guild_id].special_message.store(data, expire=t, type=name, first_only=first_only)
+    if not wait and not statistic:
+        return
+    await asyncio.sleep(t)
+    if delete_own:
+        await data.delete_reaction(reaction)
+    
+    if store_in_cache:
+        ctx.cache[data.guild_id].special_message.delete(data.id) #Not needed thanks to expire flag
+    
+    s = ctx.db.sql.Session()
+    
+    from MFramework.database.alchemy import models
+    if statistic:
+        models.Statistic.increment(s, data.guild_id, statistic)
+    if all_reactions:
+        users = await data.get_reactions(reaction)
+    
+        from MFramework.database.alchemy import items, helpers
+        item = helpers.fetch_or_add(s, items.Item, name, _type)
         i = items.Inventory(item)
         for user in users:
             helpers.add_item(s, data.guild_id, user.id, item=i)
-            models.Log.claim(data.guild_id, user.id, types.Item.EasterEgg)
-        s.commit()
-        await ctx.cache[data.guild_id].logging["egg_hunt"](data, users)
+            models.Log.claim(data.guild_id, user.id, _type)
+        await ctx.cache[data.guild_id].logging[logger](data, users)
+    s.commit()
+
+from MFramework.commands.interactions import Event, Chance
+@Event(month=4)
+@Chance(2.5)
+async def egg_hunt(ctx: Bot, data: Message):
+    await _handle_reaction(ctx, data, "🥚", "Easter Egg", types.Item.EasterEgg, logger="egg_hunt", statistic=types.Statistic.Spawned_Eggs)
 
 @Event(month=12)
+@Chance(3)
 async def present_hunt(ctx: Bot, data: Message):
-    from MFramework.database.alchemy import types
-    await _randomize(ctx, data, 3, "🎁", "Present", wait=False, delete_own=False, store_in_cache=True, first_only=True, all_reactions=False, logger="present_hunt", statistic=types.Statistic.Spawned_Presents)
-    return
-    import random
-    if random.SystemRandom().randint(1, 100) < 5:
-        import asyncio
-        await asyncio.sleep(random.SystemRandom().randint(0, 10))
-        await data.react("🎁")
-        from MFramework.database.alchemy import models, types
-        s = ctx.db.sql.Session()
-        models.Statistic.increment(s, data.guild_id, types.Statistic.Spawned_Presents)
-        t = random.SystemRandom().randint(15, 60)
-        ctx.cache[data.guild_id].special_message.store(data, expire=t, type="Present", first_only=True)
-        await asyncio.sleep(t)
-        #ctx.cache[data.guild_id].special_message.delete(data.id)
-        await data.delete_reaction("🎁")
+    await _handle_reaction(ctx, data, "🎁", "Present", wait=False, delete_own=False, store_in_cache=True, first_only=True, all_reactions=False, logger="present_hunt", statistic=types.Statistic.Spawned_Presents)
+
+@Event(month=10)
+@Chance(1)
+async def halloween_hunt(ctx: Bot, data: Message):
+    await _handle_reaction(ctx, data, "🎃", "Pumpkin", wait=False, delete_own=False, store_in_cache=True, first_only=True, all_reactions=False, logger="halloween_hunt", statistic=types.Statistic.Spawned_Pumpkins)
 
 async def responder(ctx: Bot, msg: Message, emoji: str):
     emoji = ctx.cache[msg.guild_id].custom_emojis.get(emoji.lower().strip(':'))
@@ -132,9 +88,28 @@ async def deduplicate_messages(self: Bot, data: Message) -> bool:
     return False
 
 async def roll_dice(self: Bot, data: Message, update: bool = False):
-    if data.channel_id in self.cache[data.guild_id].rpg_channels:
-        from . import dice
-        await dice.roll(self, data, update)
+    if data.channel_id not in self.cache[data.guild_id].rpg_channels:
+        return
+    if update:
+        m = await self.get_channel_message(data.channel_id, data.id)
+        if m.reactions:
+            return
+    import re
+    ILLEGAL_ACTIONS = re.compile(r"(?i)zabij|wyryw|mord")
+    DICE_REACTIONS = ['0️⃣','1️⃣','2️⃣','3️⃣','4️⃣','5️⃣','6️⃣']
+    DICE_EMOJIS = {0:'dice_0:761760091648294942',
+        1:'dice_1:761760091971780628',2:'dice_2:761760091837825075',3:'dice_3:761760092206792750',
+        4:'dice_4:761760092767911967',5:'dice_5:761760093435068446',6:'dice_6:761760093817143345'}
+    reg = re.findall(r"(?:(?=\*)(?<!\*).+?(?!\*\*)(?=\*))", data.content)
+    if reg and set(reg) != {'*'}:
+        if '*' in reg:
+            reg = set(reg)
+            reg.remove('*')
+            reg = list(reg)
+        dices = self.cache[data.guild_id].rpg_dices or DICE_REACTIONS
+        from random import SystemRandom as random
+        v = random().randint(1, 6) if not ILLEGAL_ACTIONS.findall(reg[0]) else 0
+        await self.create_reaction(data.channel_id, data.id, dices[v])
 
 async def handle_level(self: Bot, data: Message):
     if data.channel_id not in self.cache[data.guild_id].disabled_channels and not any(r in data.member.roles for r in self.cache[data.guild_id].disabled_roles):
