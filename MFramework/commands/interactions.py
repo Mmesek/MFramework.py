04c06d3026d8f18a24eb388397ae682fddadb0c5
Mmesek <13630781+Mmesek@users.noreply.github.com>
2021-05-30 01:56:29 +0200

    Refactored. Added support for Context Abstracted registering commands

--- MFramework/commands/interactions.py
+++ MFramework/commands/interactions.py
@@ -1,33 +1,30 @@
-from MFramework import (onDispatch,
-    RoleID, UserID, ChannelID, Role, User, Channel, Guild_Member, Snowflake, 
+from MFramework import (onDispatch, Ready,
+    RoleID, UserID, ChannelID, Role, User, Channel, Guild_Member, Snowflake,
     Interaction, Guild, Application_Command,
     Context, Bot, Groups, log)
 
-from ._utils import user_group, find_command, is_nested, iterate_commands, set_default_arguments
+from ._utils import is_nested, iterate_commands, set_default_arguments, commands
 
 @onDispatch
 async def interaction_create(client: Bot, interaction: Interaction):
     '''Called after receiving event INTERACTION_CREATE from Discord'''
     ctx = Context(client.cache, client, interaction)
     name = interaction.data.name
-    g = user_group(ctx)
-
-    f = find_command(g, name)
-    if not f:
-        log.debug("Command %s not found", name)
+    f = commands.get(name, None)
+    g = ctx.permission_group
+    if not f or f.group.value < g.value:
+        if not f:
+            log.debug("Command %s not found", name)
         return
-    
-    ##########TODO##FIXME###TODO################################################################
-    sub = name+'.'+interaction.data.options[0].name if len(interaction.data.options) > 0 and f['sub_commands'] != [] else name
-    #TODO: Follow the rabbit hole if sub_commands are set
+    sub = interaction.data.options[0].name if len(interaction.data.options) > 0 and f.sub_commands != [] else name
     if sub != name:
         f = is_nested(g, f, sub)
         interaction.data.options = interaction.data.options[0].options
     kwargs = {}
     for option in interaction.data.options:
-        t = f['arguments'][option.name]['type']
+        t = f.arguments[option.name].type
         if t not in [Channel, Role, User, Guild_Member, ChannelID, UserID, RoleID]:
-            kwargs[option.name] = f['arguments'][option.name]['type'](option.value)
+            kwargs[option.name] = f.arguments[option.name].type(option.value)
         elif issubclass(t, Snowflake):
             # We are casting it to Snowflake due to some recursion error. 
             # Otherwise, the above should be sufficient
@@ -37,64 +34,102 @@ async def interaction_create(client: Bot, interaction: Interaction):
             if t is Guild_Member:
                 o.user = interaction.data.resolved.users.get(str(option.value), User())
             kwargs[option.name] = o
-        if t is RoleID:
-            print(issubclass(t, Snowflake))
-    ############################################################################################
-    options = {i.name:f['arguments'] for i in interaction.data.options if i.name not in kwargs}
-
     kwargs = set_default_arguments(ctx, f, kwargs)
-    await f['function'](client, interaction=interaction, language='en', group=g, **kwargs)
+    await f.func(ctx=ctx, client=client, interaction=interaction, language='en', group=g, **kwargs)
+
+from mlib import arguments
+CLEAR_INTERACTIONS = arguments.parse().clear_interactions
 
 
-import sys
 @onDispatch
-async def ready(client: Bot):
+async def ready(client: Bot, ready: Ready):
     '''Called after connecting with Discord. Preferably after receiving READY event'''
     if getattr(client, 'registered', False):
         return
     client.application = await client.get_current_bot_application_information()
-
-    registered = await client.get_global_application_commands(client.application.id)
-
-    if '--clear_interactions' in sys.argv:
-        for cmd in registered:
-            log.info("Deleting Global command %s from bot %s", cmd, client.username)
-            await client.delete_global_application_command(client.application.id, cmd.id)
-        return
-    
-    new_commands = []
-    for command, _command, options in iterate_commands(registered):
-        if not _command['interaction'] or _command['guild'] or _command.get('master_command', False):
-            log.debug("Skippging command %s from registering as Global. Eiter Guild-Only or it's a subCommand", command)
-            continue
-        log.info("Registering Global command %s on bot %s", command, client.username)
-        new_commands.append(Application_Command(name=command, description=_command['help'][:100], options=options, default_permission=_command['group'] == Groups.GLOBAL))
-    if new_commands != []:
-        await client.bulk_overwrite_global_application_commands(client.application.id, new_commands)
+    await register_commands(client)
 
 @onDispatch
 async def guild_create(client: Bot, guild: Guild):
     '''Called after GUILD_CREATE'''
-    try:
-        app = client.application
-    except:
+    if not client.application:
         import asyncio
         await asyncio.sleep(3)
-        app = client.application
+        return await guild_create(client, guild)
+    
+    _commands = await register_commands(client, guild)
+    
+    #from ._utils import set_permissions
+    #await set_permissions(client, guild.id, client.registered_commands + _commands)
 
-    registered = await client.get_guild_application_commands(app.id, guild.id)
+async def register_commands(client: Bot, guild: Guild = None):
+    registered = await get_commands(client, guild)
 
-    if '--clear_interactions' in sys.argv:
-        for cmd in registered:
-            log.info("Deleting Guild command %s from bot %s from %s", cmd.name, client.username, guild.id)
-            await client.delete_guild_application_command(client.application.id, guild.id, cmd.id)
-        return
+    registered_commands = {i.name: i.id for i in registered}
 
     new_commands = []
-    for command, _command, options in iterate_commands(registered):
-        if not _command['interaction'] or _command['guild'] != guild.id or _command.get('master_command', False):
-            continue
-        log.info("Registering Guild command %s on bot %s for %s", command, client.username, guild.id)
-        new_commands.append(Application_Command(name=command, description=_command['help'][:100], options=options, default_permission=_command['group'] == Groups.GLOBAL))
-    if new_commands != []:
-        await client.bulk_overwrite_guild_application_commands(client.application.id, guild.id, new_commands)
+    updated_commands = []
+    _cmds = []
+
+    if CLEAR_INTERACTIONS:
+        return await overwrite_commands(client, [], guild)
+
+    for command, _command, options in iterate_commands(registered, guild.id if guild else None):
+        cmd = Application_Command(name=command, description=_command.help[:100], options=options, default_permission=_command.group == Groups.GLOBAL)
+        if command not in registered_commands:
+            new_commands.append(cmd)
+        else:
+            cmd.id = registered_commands[cmd.name]
+            updated_commands.append(cmd)
+
+
+    if len(new_commands) == len(commands):
+        _cmds = await overwrite_commands(client, new_commands + updated_commands, guild)
+    else:
+        for cmd in new_commands:
+            _cmds.append(await add_command(client, cmd, guild))
+        for cmd in updated_commands:
+            _cmds.append(await edit_command(client, cmd, guild))
+
+    if guild:
+        _cmds = registered + _cmds
+        #return _cmds
+        if len(new_commands) > 0:
+            from ._utils import set_permissions
+            await set_permissions(client, guild.id, client.registered_commands+_cmds)
+    else:
+        client.registered_commands = registered
+
+from typing import List
+async def get_commands(client: Bot, guild: Guild = None) -> List[Application_Command]:
+    if guild:
+        return await client.get_guild_application_commands(client.application.id, guild.id)
+    return await client.get_global_application_commands(client.application.id)
+
+async def add_command(client: Bot, cmd: Application_Command, guild: Guild = None) -> Application_Command:
+    if guild:
+        log.info("Registering Guild [%s] command %s on bot %s", guild.id, cmd.name, client.username)
+        return await client.create_guild_application_command(client.application.id, guild.id, cmd.name, cmd.description, cmd.options, cmd.default_permission)
+    log.info("Registering Global command %s on bot %s", cmd.name, client.username)
+    return await client.create_global_application_command(client.application.id, cmd.name, cmd.description, cmd.options, cmd.default_permission)
+
+async def edit_command(client: Bot, cmd: Application_Command, guild: Guild = None) -> Application_Command:
+    if guild:
+        log.info("Editing existing Guild [%s] command %s on bot %s", guild.id, cmd.name, client.username)
+        return await client.edit_guild_application_command(client.application.id, guild.id, cmd.id, cmd.name, cmd.description, cmd.options, cmd.default_permission)
+    log.info("Editing existing Global command %s on bot %s", cmd.name, client.username)
+    return await client.edit_global_application_command(client.application.id, cmd.id, cmd.name, cmd.description, cmd.options, cmd.default_permission)
+
+async def delete_command(client: Bot, cmd: Application_Command, guild: Guild = None) -> None:
+    if guild:
+        log.info("Deleting Guild [%s] command %s from bot %s", guild.id, cmd.name, client.username)
+        return await client.delete_guild_application_command(client.application.id, guild.id, cmd.id)
+    log.info("Deleting Global command %s from bot %s", cmd.name, client.username)
+    return await client.delete_global_application_command(client.application.id, cmd.id)
+
+async def overwrite_commands(client: Bot, commands: List[Application_Command]=[], guild: Guild = None) -> List[Application_Command]:
+    if guild:
+        log.info("Overwriting Guild [%s] commands [%s] on bot %s", guild.id, len(commands), client.username)
+        return await client.bulk_overwrite_guild_application_commands(client.application.id, guild.id, commands)
+    log.info("Overwriting Global commands [%s] on bot %s", len(commands), client.username)
+    return await client.bulk_overwrite_global_application_commands(client.application.id, commands)
