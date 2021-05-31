e6c0ad3ae8dc05867b5d0b93d17dd73574744eca
Mmesek <13630781+Mmesek@users.noreply.github.com>
2021-05-31 02:20:08 +0200

    Ported statistics to slash commands

--- commands_slash/statistics.py
+++ commands_slash/statistics.py
@@ -1,32 +1,36 @@
-from MFramework.commands import register
-from MFramework.utils.utils import Embed, convert_bytes, getsize, file_size, secondsToText, bytes2human
-import asyncio, time
+from MFramework import register, Groups, Context, Interaction, Embed
+import time
 
+@register(group=Groups.GLOBAL, guild=463433273620824104)
+async def bot(ctx: Context, interaction: Interaction, *args, language, **kwargs):
+    '''
+    Shows Information about bot
+    '''
+    pass
 
-@register(group='Global', help='Shows ping & Heartbeat latency', alias='', category='')
-async def ping(self, *args, data, detailed=False, language, **kwargs):
-    '''Extended description to use with detailed help command'''
+@register(group=Groups.GLOBAL, guild=463433273620824104, main=bot)
+async def ping(ctx: Context, interaction: Interaction, detailed: bool=False, *args, language, **kwargs):
+    '''
+    Shows ping
+    Params
+    ------
+    detailed:
+        Whether it should show extended ping information
+    '''
     s = time.time()
-    m = await self.message(data.channel_id, "Pong!")
+    await interaction.deferred_message()
     end = time.time()
+    e = None
     if detailed:
-        from datetime import datetime, timezone
-        now = datetime.now(tz=timezone.utc)
-        t = datetime.fromisoformat(data.timestamp)
-        msg_t = datetime.fromisoformat(m["timestamp"])
-        internal = now - t
-        msg_diff = msg_t - t
         discord = ping()
         e = Embed().addField("Discord", f"{discord}", True)
         router = ping('192.168.1.254')
         if router[0] != '0':
             e.addField("Router", f"{router}", True)
-        if self.latency != None:
-            e.addField("Heartbeat", "{0:.2f}ms".format(self.latency), True)
-        e.setFooter(None, "Internal | Timestamp Difference: {} | {}s".format(internal.total_seconds(), msg_diff.total_seconds()))
-        await self.edit_message(data.channel_id, m["id"], f"Pong! `{int((end-s)*1000)}ms`", e.embed)
-    else:
-        await self.edit_message(data.channel_id, m["id"], f"Pong! `{int((end-s)*1000)}ms`")
+        if ctx.bot.latency != None:
+            e.addField("Heartbeat", "{0:.2f}ms".format(ctx.bot.latency), True)
+        e = [e]
+    await interaction.edit_response(f"Pong! `{int((end-s)*1000)}ms`", e)
 
 def ping(host='discord.com'):
     import platform, os
@@ -47,11 +51,20 @@ def ping(host='discord.com'):
         ping+='ms'
     return ping.lstrip().strip('\n')
 
-
-@register(group="System", help="Shows statistics related to bot and system")
-async def status(self, *args, data, no_ping=False, language, **kwargs):
-    import psutil
-    self_size = getsize(self)
+@register(group=Groups.GLOBAL, guild=463433273620824104, main=bot)
+async def status(ctx: Context, interaction: Interaction, show_ping: bool=False, *args, language="en", **kwargs):
+    '''
+    Shows statistics related to bot and system
+    Params
+    ------
+    show_ping:
+        whether it should show ping or not
+    '''
+    await interaction.deferred_message()
+    from mlib.sizes import getsize, bytes2human, convert_bytes, file_size
+    from mlib.localization import secondsToText
+    import psutil, asyncio
+    self_size = getsize(ctx.bot)
     embed = Embed()
     tasks = [t for t in asyncio.all_tasks() if t is not asyncio.current_task()]
     cpu = psutil.cpu_percent()
@@ -66,14 +79,14 @@ async def status(self, *args, data, no_ping=False, language, **kwargs):
 
     embed.addField("Uptime", secondsToText(sys_uptime, language), True)
     embed.addField("Bot Uptime", secondsToText(proc_uptime, language), True)
-    embed.addField("Session", secondsToText(int(time.time() - self.startTime), language), True)
-    if not no_ping:
+    embed.addField("Session", secondsToText(int(time.time() - ctx.bot.start_time), language), True)
+    if show_ping:
         discord = ping()
         api = 0#ping("")
         cdn = ping("cdn.discordapp.com")
         embed.addField("Ping", f"Discord: {discord}\nAPI: {api}ms\nCDN: {cdn}", True)
-    if self.latency != None:
-        embed.addField("Latency", "{0:.2f}ms".format(self.latency), True)
+    if ctx.bot.latency != None:
+        embed.addField("Latency", "{0:.2f}ms".format(ctx.bot.latency), True)
     else:
         embed.addField("\u200b", "\u200b", True)
     embed.addField("Current Temperature", "{0:.2f}'C".format(temp), True)
@@ -86,11 +99,7 @@ async def status(self, *args, data, no_ping=False, language, **kwargs):
     except:
         child_usage = 0
         self_usage = 0
-    #    print(resource.getrusage(resource.RUSAGE_CHILDREN))
-    #    print(resource.getrusage(resource.RUSAGE_SELF))
     network = psutil.net_io_counters(pernic=True)
-    eth = 0
-    wlan = 0
     net = ''
     if 'eth0' in network:
         ethdl = bytes2human(network['eth0'].bytes_recv)
@@ -109,9 +118,9 @@ async def status(self, *args, data, no_ping=False, language, **kwargs):
     embed.addField("Network Usage",net, True)
     embed.addField("RAM Usage", convert_bytes(self_usage + child_usage), True)
 
-    cache_size = getsize(self.cache)
-    if hasattr(self, "index"):
-        idx = getsize(self.index)
+    cache_size = getsize(ctx.bot.cache)
+    if hasattr(ctx, "index"):
+        idx = getsize(ctx.index)
         bot_size = convert_bytes(self_size - cache_size - idx)
         idx = "File: " + file_size("data/steamstoreindex.json") + "\nLoaded: " + convert_bytes(idx)
     else:
@@ -129,14 +138,18 @@ async def status(self, *args, data, no_ping=False, language, **kwargs):
     except:
         embed.addField("\u200b", "\u200b", True)
     embed.addField("Cache Size", convert_bytes(cache_size), True)
-    #embed.addField("\u200b", "\u200b", True)
     embed.addField("Steam Index Size", idx, True)
-    embed.setColor(self.cache[data.guild_id].color)
-    await self.embed(data.channel_id, "", embed.embed)
+    r = await ctx.bot.get_gateway_bot()
+    embed.addField("Remaining sessions", r.get('session_start_limit', {}).get('remaining', -1))
+    embed.setColor(ctx.cache.color)
+    await interaction.edit_response(embeds=[embed])
 
-
-@register(group="System", help="Shows bot information")
-async def version(self, *args, data, **kwargs):
+@register(group=Groups.GLOBAL, guild=463433273620824104, main=bot)
+async def version(ctx: Context, interaction: Interaction, *args, language, **kwargs):
+    '''
+    Shows bot's version
+    '''
+    await interaction.deferred_message()
     import platform
     system = platform.system()
     release = platform.release()
@@ -144,17 +157,17 @@ async def version(self, *args, data, **kwargs):
     node = platform.node()
     arch = platform.architecture()
     python = platform.python_version()
-    shard = self.shards
-    servers = len(self.cache)
+    shard = ctx.bot.shards
+    servers = len(ctx.bot.cache)
     from MFramework import __version__ as ver, ver_date
-    mframework = ver#self.version
-    seq = self.last_sequence
-    desc = f"{self.username} @ {node}"
+    mframework = ver
+    seq = ctx.bot.last_sequence
+    desc = f"{ctx.bot.username} @ {node}"
     if 'arm' in machine:
         from gpiozero import pi_info
         node = 'Raspberry {0}\n{1}MB'.format(pi_info().model, pi_info().memory)
     try:
-        influx = await self.db.influx.influxPing()
+        influx = await ctx.db.influx.influxPing()
     except:
         influx = "ERROR: Database offline."
     try:
@@ -177,37 +190,28 @@ async def version(self, *args, data, **kwargs):
     .addField("\u200b", "\u200b", True)
     .addField("PostgreSQL", postgres, True)
     )
-    embed.setTimestamp(ver_date).setFooter('',"Last Commit")
-    embed.setColor(self.cache[data.guild_id].color).setDescription(desc)
-    await self.embed(data.channel_id, "", embed.embed)
+    embed.setTimestamp(ver_date).setFooter("Last Commit")
+    embed.setColor(ctx.cache.color).setDescription(desc)
+    await interaction.edit_response(embeds=[embed])
 
-@register(group='System', help='Short description to use with help command', notImpl=True)
-async def remaining(self, *args, data, **kwargs):
-    '''Extended description to use with detailed help command'''
-    print(await self.get_gateway_bot())
-
-@register(group='System', help='Short description to use with help command', alias='', category='')
-async def stats(self, *args, data, language, **kwargs):
-    '''Extended description to use with detailed help command'''
+@register(group=Groups.GLOBAL, guild=463433273620824104, main=bot)
+async def stats(ctx: Context, interaction: Interaction, *args, language, **kwargs):
+    '''
+    Shows received events & registered commands
+    '''
     msg = ''
     e = Embed()
-    for counter in self.counters:
-        msg += f"\n`{counter}`: {self.counters[counter]}"
+    for counter in ctx.bot.counters:
+        msg += f"\n`{counter}`: {ctx.bot.counters[counter]}"
 
-    from MFramework.commands import commandList
-    groups = list(commandList)
+    from MFramework.commands._utils import commands
+    groups = list(commands)
     groups.reverse()
     cmds = ""
-    for x, group in enumerate(groups):
-        if x != 0:
-            l = set(commandList[groups[x - 1]].values())
-            l = len(l)
-        else:
-            l = 0
-        c = set(commandList[group].values())
-        cmds += f"\n`{group}`: {len(c)-l}"
-    cmds += f"\n`Total`: {len(set(commandList['System'].values()))}"
+    cmds += f"\n`Total`: {len(set(commands))}"
+    #TODO: It doesn't list groups or subcommands yet!
+    #TODO: Show executed commands?
 
     e.addField("Events Received", msg, True)
-    e.addField("Registered Commands", cmds, True)    
-    await self.embed(data.channel_id, "", e.embed)
+    e.addField("Registered Commands", cmds, True)
+    await interaction.respond_private(embeds=[e])
