6fc11e992afb5b1ccf9e938128f0456619a13c50
Mmesek <13630781+Mmesek@users.noreply.github.com>
2021-06-22 18:48:53 +0200

    Added init for simpler importing

---
+++ MFramework/commands/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+'''
+Commands
+----------
+
+Command registery & execution framework
+
+:copyright: (c) 2021 Mmesek
+
+'''
+from ._utils import Groups # noqa: F401
+from .decorators import * # noqa: F401
