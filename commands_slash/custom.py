a956927f4805cb2e80477e46754e012ae15e5484
Mmesek <13630781+Mmesek@users.noreply.github.com>
2021-07-05 19:04:47 +0200

    Added command to randomize game loadout

--- commands_slash/custom.py
+++ commands_slash/custom.py
@@ -54,3 +54,106 @@ async def bookmark(ctx: Context, *title: str, message: Message, language, **kwar
     dm = await ctx.bot.create_dm(ctx.user_id)
     title = title[0] or "Your bookmark"
     await ctx.bot.create_message(dm.id, title+': \n'+Discord_Paths.MessageLink.link.format(guild_id=ctx.guild_id, channel_id=ctx.channel_id, message_id=ctx.message_id))
+
+@register(group=Groups.GLOBAL, guild=340185368655560704)
+async def loadout(ctx: Context, interaction: Interaction, *args, language, **kwargs):
+    '''
+    Losuje ekwipunek
+    '''
+    eq = {
+        "primary_weapon": [
+            "R-201", "R-101", "Cykuta BF-R", "G2A5", "V-47 Kostucha",
+            "CAR", "Alternator", "Wolt", "R97",
+            "Spitfire", "L-STAR", "X-55 Oddanie",
+            ["Kraber-AP", {"Szybki i Wściekły": "Rykoszet"}], ["D-2 Dublet", {"Szybki i Wściekły": "Rykoszet"}], "Longbow-DMR",
+            "EVA-8 Auto", "Mastiff",
+            "Grzechotnik SMR", "EPG-1", "R-6P Softball", "EM-4 Zimna Wojna",
+            ["Elitarny Skrzydowy", {"Zmiana Broni": "Rykoszet"}], "SA-3 Mozambik"
+        ],
+        "primary_mods": ["Szybkie Przeładowanie", "Szybki i Wściekły", "Rewolwerowiec", "Zmiana Broni", "Taktyczna Eliminacja", "Dodatkowa Amunicja"],
+        "secondary_weapon": ["RE-45 Auto", "Hammond P2016", "Skrzydłowy B3"],
+        "secondary_mods": ["Szybkie Przeładowanie", "Tłumik", "Szybki i Wściekły", "Rewolwerowiec", "Taktyczna Eliminacja", "Dodatkowa Amunicja"],
+        "anti_titan_weapon": [["Karabin Ładunkowy",{"Szybkie Przeładowanie": "Hakowanie Ładowania"}]],#, "Wyrzutnia Magnetyczna WGM", "LG-97 Grom", "Łucznik"],
+        "anti_titan_mods": ["Szybkie Przeładowanie", "Rewolwerowiec", "Dobycie Broni", "Dodatkowa Amunicja"],
+        "ordnance": ["Odłamkowy", "Łukowy", "Gwiazda Ognista", "Gwiazda Grawitacyjna", "Elektro-Dymny", "Ładunek Wybuchowy"],
+        "booster": ["Wzmocniona Broń", "Kleszcze", "Wieżyczka Przeciw Pilotom", "Hakowanie Mapy", "Zapasowa Bateria", "Zakłócacz Radaru", "Wieżyczka Przeciw Tytanom", "Pistolet Cyfrowy", "Przewinięcie Fazowe", "Twarda Osłona", "Multi Holopilot", "Rzut Kostką"],
+
+        "faction": ["Korpus Łupieżców", "Łowcy Alfa", "Vinson Dynamics", "Elita Miasta Aniołów", "6-4", "Dywizja ARES", "Marvińska Elita"],
+
+        "pilot_tactical": ["Maskowanie", "Ostrze Pulsacyjne", "Lina z Hakiem", "Stymulant", "Ściana Energetyczna", "Przeskok Fazowy", "Holopilot"],
+        "pilot_kit": ["Bateria", "Szybka Regeneracja", "Gadżeciarz", "Wejście Fazowe"],
+        "pilot_kit_2": ["Raport o Poległych", "Ścianołaz", "Zawis", "Cichociemny", "Łowca Tytanów"],
+
+        "titan": {
+            "Kationa":["Splątana Energia", "Mina Wiązkowa", "Wzmacniacz Wirowy", "Megadziałko", "Soczewka Refrakcyjna"], 
+            "Spopielacz":["Pożoga", "Wzmocnione Poszycie", "Piekielna Tarcza", "Zapas Paliwa", "Spalona Ziemia"], 
+            "Polaris":["Strzał Penetrujący", "Zwiększony Ładunek", "Podwójne Pułapki", "Silniki pomocnicze Żmija", "Optyka Detektorowa"], 
+            "Ronin":["Naboje Rykoszetujące", "Gromowładny", "Anomalia Czasowa", "Fechmistrz", "Unik Fazowy"], 
+            "Ton":["Pociski Śledzące +", "Wzmocniona Ściana Energetyczna", "Echo Pulsowe", "Rakietowa Salwa", "Zasobnik Seryjny"], 
+            "Legion":["Większy Zasób Amunicji", "Siatka Czujników", "Zapora", "Lekkie Materiały", "Ukryta Komora"], 
+            "Monarcha":["Wzmacniacz Tarczy", "Złodziej Energii", "Szybkie Przezbrojenie", "Przetrwają Najsilniejsi"]
+        },
+        "titan_kit": ["Chip Szturmowy", "Katapulta z Maskowaniem", "Silnik Turbo", "Doładowanie Rdzenia", "Nuklearna Katapulta", "Gotowa Riposta"],
+        "titanfall_kit": ["Tarcza Sklepieniowa", "Hiperzrzut"],
+
+        "mode": {"Wyniszczenie": True, "Starcie Pilotów": True, "Obrona Umocnień": False, "Łowy": True, "Walka o Flagę": False, "Do Ostatniego Tytana": True, "Piloci Kontra Piloci": False, "Szybki Szturm": False, "Na Celowniku": False, "Starcie Tytanów": True, "Obrona Kresów": True, "Koloseum": True},
+        "map": {"Baza Kodai": True, "Wybuchowo": False, "Gospodarstwo": True, "Egzoplaneta": True, "Kanał ,,Czarna Toń''": True, "Eden": False, "Suchy Dok": True, "Miejsce Katastrofy": False, "Kompleks": False, "Miasto Aniołów": True, "Kolonia": False, "Błąd": False, "Relikt": False, "Gry Wojenne": True, "Powstanie": True, "Stosy": False, "Pokład": False, "Łąka": False, "Ruch Uliczny": False, "Miasto": False, "UMA": False}
+    }
+    cores = {
+        1: ["Pociski Łukowe", "Zasobink Rakietowy", "Transfer Energii"],
+        2: ["Przezbrojenie i Przeładowanie", "Wir", "Pole Energetyczne"],
+        3: ["Wielokrotne Namierzanie", "Udoskonalony Kadłub", "Akcelerator XO-16"]
+    }
+
+    from random import SystemRandom
+    random = SystemRandom()
+
+    r = {}
+    mods_overwrite = {}
+    for k, v in eq.items():
+        if type(v) is dict:
+            _v = [i for i in v.keys()]
+        else:
+            _v = v
+        if 'mods' in k:
+            c = random.sample(_v, k=2)
+            mods = []
+            for i in c:
+                if i in mods_overwrite:
+                    i = mods_overwrite[i]
+                mods.append(i)
+            mods_overwrite = {}
+
+            c = "\n- ".join([""]+mods)
+            r[k.replace("mods", "weapon")] += c
+            continue
+        else:
+            c = random.choice(_v)
+        if type(c) is list:
+            mods_overwrite = c[1]
+            c = c[0]
+        if k == 'map':
+            if eq['mode'][r['mode']]:
+                if r['mode'] == 'Obrona Kresów':
+                    maps = [i[0] for i in filter(lambda i: i[1], eq['map'].items())]
+                else:
+                    maps = [i for i in eq['map'].keys()][:-6]
+                c = random.choice(maps)
+
+        r[k] = c
+        if c == 'Monarcha':
+            for level, core in cores.items():
+                r[f"core_{level}"] = random.choice(core)
+
+        if k == 'titan':
+            c = random.choice(v[c])
+            r['class_kit'] = c
+        if k == 'mode' and r['mode'] == 'Koloseum':
+            break
+
+    embed = Embed()
+    from mlib.localization import tr
+    for k, v in r.items():
+        embed.addField(tr("commands.loadout."+k, language='pl'), v, True)
+
+    await interaction.send(None, [embed])
