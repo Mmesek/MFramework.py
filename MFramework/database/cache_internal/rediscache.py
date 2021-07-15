66775361a1f5ed793fc99a629556481414999f94
Mmesek <13630781+Mmesek@users.noreply.github.com>
2021-07-15 17:52:52 +0200

    Cleanup

--- MFramework/database/cache_internal/rediscache.py
+++ MFramework/database/cache_internal/rediscache.py
@@ -1,25 +1,7 @@
 from datetime import timedelta
-'''
-from redisworks import Root
-
-r = Root(host='192.168.1.5')
-class Guild:
-    pass
-g = Guild()
-g.id = 1
-#r.guild = {1:{}}
-#r.guild.s123 = {}
-#r.guild[123] = {"id":1, "roles":[123,234,345]}
-print(r.guild[123])
-class Member:
-    pass
-m = Member()
-m.user = 1
-m.roles = [1,2,3]
-
-r[123].member.s3 = m
-'''
 import redis
+
+
 class RDS:
     r: redis.Redis
     def __init__(self) -> None:
