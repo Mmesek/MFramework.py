67c877fd59c3054ed9b92341c17d44da94c572f4
Mmesek <13630781+Mmesek@users.noreply.github.com>
2021-07-15 16:52:23 +0200

    Added missing typehints

--- MFramework/utils/timers.py
+++ MFramework/utils/timers.py
@@ -1,50 +1,55 @@
 import time
+from typing import Tuple
+
+from MFramework import Snowflake
+from MFramework.bot import Bot
 from MFramework.database.alchemy import log, models, types
 from MFramework import log as _log
-def checkLast(self, guild, channel, user):
-    j = self.cache[guild].voice[channel].pop(user)
+
+def checkLast(ctx: Bot, guild: Snowflake, channel: Snowflake, user: Snowflake) -> float:
+    j = ctx.cache[guild].voice[channel].pop(user)
     if j > 0:
         n = time.time()
     else:
         return 0
     return n - j
 
-def finalize(self, guild, channel, user):
+def finalize(ctx: Bot, guild: Snowflake, channel: Snowflake, user: Snowflake) -> Tuple[float, Tuple[Snowflake, float]]:
     _v = 0
-    v = checkLast(self, guild, channel, user)
+    v = checkLast(ctx, guild, channel, user)
     if v != 0:
-        session = self.db.sql.session()
+        session = ctx.db.sql.session()
         _user = models.User.fetch_or_add(session, id=user)
         stat = log.Statistic.get(session, guild, user, types.Statistic.Voice)
         stat.value += int(v)
         session.commit()
-        if self.cache[guild].is_tracking(types.Flags.Activity):
-            self.db.influx.commitVoiceSession(guild, channel, user, v)
+        if ctx.cache[guild].is_tracking(types.Flags.Activity):
+            ctx.db.influx.commitVoiceSession(guild, channel, user, v)
     _log.debug(f'Removed {user} from {channel} after {v}')
-    in_channel = self.cache[guild].voice[channel]
+    in_channel = ctx.cache[guild].voice[channel]
     if len(in_channel) == 1:
         user = list(in_channel.keys())[0]
         _log.debug(f'reStarting alone {user}')
         #finalize(self, guild, channel, user)
-        _v = restartTimer(self, guild, channel, user)
+        _v = restartTimer(ctx, guild, channel, user)
     return v, (user, _v)
 
-def startTimer(self, guild, channel, user):
-    if self.cache[guild].is_tracking(types.Flags.Voice):
+def startTimer(ctx: Bot, guild: Snowflake, channel: Snowflake, user: Snowflake) -> None:
+    if ctx.cache[guild].is_tracking(types.Flags.Voice):
         t = time.time()
     else:
         t = 0
-    self.cache[guild].voice[channel][user] = t
+    ctx.cache[guild].voice[channel][user] = t
     if t > 0:
         _log.debug(f'Starting Timer for {user} in {channel}')
 
-def restartTimer(self, guild, channel, user, flag=0):
-    c = self.cache[guild].voice[channel]
+def restartTimer(ctx: Bot, guild: Snowflake, channel: Snowflake, user: Snowflake, flag: int=0) -> float:
+    c = ctx.cache[guild].voice[channel]
     v = (0,0)
     if user in c:
         if c[user] > 0:
             _log.debug(f'Finalizing Previous Timer for {user} in {channel}')
-            v = finalize(self, guild, channel, user)
+            v = finalize(ctx, guild, channel, user)
     c[user] = flag
     _log.debug(f'reStarting Timer for {user} in {channel} with {flag}')
     return v[0]
