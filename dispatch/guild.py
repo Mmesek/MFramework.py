87a4d41937252ae1d78787ad0b0fd1bed1788df6
Mmesek <13630781+Mmesek@users.noreply.github.com>
2021-07-20 02:15:57 +0200

    - Changed guild_member_add to update from store - Changed logging guild init to log instead of print - Increase min allowed amount of members in guild before fetching all

--- dispatch/guild.py
+++ dispatch/guild.py
@@ -1,4 +1,4 @@
-from MFramework import onDispatch, Bot, Guild, Guild_Member_Add, Guild_Member_Remove, Guild_Members_Chunk
+from MFramework import onDispatch, Bot, Guild, Guild_Member_Add, Guild_Member_Remove, Guild_Members_Chunk, log
 
 @onDispatch
 async def guild_create(self: Bot, guild: Guild):
@@ -9,15 +9,15 @@ async def guild_create(self: Bot, guild: Guild):
         self.cache[guild.id] = GuildCache(self, guild)
         from MFramework.utils.scheduler import add_guild_tasks
         add_guild_tasks(self, guild.id)
-        print('Guild', guild.id, 'initialized in:', time.time() - start)
-    if len(self.cache[guild.id].members) < 10:
+        log.info("Guild %s initialized in %s", guild.id, time.time() - start)
+    if len(self.cache[guild.id].members) < 50:
         await self.request_guild_members(guild.id)
 
 @onDispatch
 async def guild_member_add(self: Bot, data: Guild_Member_Add):
     await self.db.influx.influxMember(data.guild_id, data.user.id, True, data.joined_at)
     await self.cache[data.guild_id].logging["join"](data)
-    self.cache[data.guild_id].members.store(data)
+    self.cache[data.guild_id].members.update(data)
 
 @onDispatch
 async def guild_member_remove(self: Bot, data: Guild_Member_Remove):
