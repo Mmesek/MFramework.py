5cc148df9bfbb738ebe03554dc7f8141b2abf270
Mmesek <13630781+Mmesek@users.noreply.github.com>
2021-08-02 21:17:57 +0200

    Moved Listmembers to info & improved displaying Snowflake creation data Also removed unnecessary duplicate creationdate command

--- commands_slash/info.py
+++ commands_slash/info.py
@@ -1,4 +1,4 @@
-from MFramework import register, Groups, Context, Embed, UserID, Snowflake, RoleID, ChannelID
+from MFramework import register, Groups, Context, Embed, UserID, Snowflake, RoleID, ChannelID, Role
 
 @register()
 async def info(ctx: Context, *args, language, **kwargs):
@@ -118,8 +118,33 @@ async def channel(ctx: Context, channel_id: ChannelID = 0, *args, language, grou
 
     await ctx.reply(embeds=[embed])
 
+@register(group=Groups.ADMIN, main=info)
+async def members(ctx: Context, role: Role, force_update: bool=False, *args, language, **kwargs):
+    '''
+    Lists users with provided role
+    Params
+    ------
+    role:
+        Role to fetch
+    force_update:
+        Whether new users should be pulled before counting (Delays response by around 30 seconds)
+    '''
+    await ctx.deferred(private=False)
+    if force_update:
+        await ctx.bot.request_guild_members(ctx.guild_id)
+        import asyncio
+        await asyncio.sleep(30)
+    total = []
+    for member_id, member in ctx.cache.members.items():
+        if role.id in member.roles:
+            total.append(member_id)
+    from MFramework import Embed
+    embed = Embed().setDescription(''.join([f'<@{i}>' for i in total])).setFooter(f'Total users/members: {len(total)}/{len(ctx.cache.members)}')
+    embed.setColor(role.color).setTitle(f'List of members with role {role.name}')
+    await ctx.reply(embeds=[embed])
+
 @register(group=Groups.GLOBAL, main=info)
-async def created(ctx: Context, snowflake: Snowflake, *args, language, **kwargs):
+async def created(ctx: Context, snowflake: Snowflake, *, language):
     '''
     Shows when the snowflake was created
     Params
@@ -128,17 +153,31 @@ async def created(ctx: Context, snowflake: Snowflake, *args, language, **kwargs)
         Snowflake to check
     '''
     await ctx.deferred()
-    embed = Embed().setFooter('Dates are in format YYYY/MM/DD HH:MM UTC')
-    import time
-    r = '\nOn Discord since: ' + str(time.strftime("%Y-%m-%d %H:%M", snowflake.as_date.timetuple()))
+    names = []
+    _member, _channel, _role = None, None, None
     try:
-        member = await ctx.bot.get_guild_member(ctx.guild_id, snowflake)
-        r += '\nJoined Server at: ' + str(time.strftime("%Y-%m-%d %H:%M", member.joined_at.timetuple()))
+        _member = await ctx.bot.get_guild_member(ctx.guild_id, snowflake)
+    except:
+        _role = [i for i in ctx.cache.roles if i == snowflake]
+        if _role:
+            names.append(("Role", f"<@&{snowflake}>"))
+        _channel = [i for i in ctx.cache.channels if i == snowflake]
+        if _channel:
+            names.append(("Channel", f"<#{snowflake}>"))
+    if not _role and not _channel:
+        names.append(("User", f"<@{snowflake}>"))
+    names.append(("On Discord since", snowflake.styled_date()))
+    if _member:
+        import time
+        names.append(("Joined Server at", f"<t:{int(time.mktime(_member.joined_at.timetuple()))}>"))
         try:
-            r += '\nBooster since: ' + str(time.strftime("%Y-%m-%d %H:%M", member.premium_since.timetuple()))
+            names.append(("Booster since", f"<t:{int(time.mktime(_member.premium_since.timetuple()))}>"))
         except:
             pass
-    except:
-        pass
-    embed.addField("\u200b",f"<@{snowflake}>"+r)
+    longest_string = max([len(i[0]) for i in names])
+    r = []
+    for name, format in names:
+        r.append(f"`{name:>{longest_string}}`: {format}")
+    embed = Embed()
+    embed.addField(f"{snowflake}",'\n'.join(r))
     await ctx.reply(embeds=[embed])
