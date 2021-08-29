4fa7debd9d50c1b584fd29fccc524bddc2428cee
Mmesek <13630781+Mmesek@users.noreply.github.com>
2021-08-29 23:37:23 +0200

    Moved to separate repository at Mmesek/MBot.py

--- dispatch/threads.py
+++
@@ -1,5 +0,0 @@
-from MFramework import onDispatch, Bot, Channel
-
-@onDispatch
-async def thread_create(self: Bot, data: Channel):
-    self.cache[data.guild_id].threads[data.id] = data.parent_id
--- mbot.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd "$( cd "$( dirname "$0" )" && pwd )"
-/usr/bin/screen -dm -S "MFramework" /usr/bin/python3.7 -m MFramework
