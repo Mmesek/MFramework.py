03c7a216dcc8de9d4010972499644eee24bef6b6
Mmesek <13630781+Mmesek@users.noreply.github.com>
2021-09-01 11:33:55 +0200

    Dehardcoded commands & config paths

--- MFramework/__main__.py
+++ MFramework/__main__.py
@@ -7,19 +7,20 @@ arguments.add("-clear_guild_interactions", action='store_true', help="Specifies 
 arguments.add("-generate_translations", action='store_true', help="Specifies whether translation file should be generated")
 arguments.add("-update_translation", action='store_true', help="Specifies whether translation file should be updated")
 arguments.add("-update-permissions", action="store_true", help="Specifies whether permissions should be forced to update in guilds")
+arguments.add("paths", nargs="*", help="List of relative paths to import", default="bot")
+arguments.add("--cfg", help="Specifies relative path to config", default="secrets.ini")
 
 import MFramework
 from mlib.import_functions import import_from
-import_from('dispatch')
-import_from('commands_slash')
-#import_from('commands')
+for path in arguments.parse().paths:
+    import_from(path)
 
 if '-generate-translation' in sys.argv or '-update-translation' in sys.argv:
     exit()
 
 from mlib.config import ConfigToDict
 from os.path import dirname, realpath
-path = dirname(realpath('__file__'))+"/data/secrets.ini"
+path = dirname(realpath('__file__'))+ f"/{arguments.parse().cfg}"
 cfg = ConfigToDict(path)
 
 db = MFramework.Database(cfg)
