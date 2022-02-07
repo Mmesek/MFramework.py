b82b715febee0ff8d993a6476252419611d465c0
Mmesek <13630781+Mmesek@users.noreply.github.com>
2022-02-07 23:02:32 +0100

    Add attempting to retrieve member from cache as a fallback

--- MFramework/context.py
+++ MFramework/context.py
@@ -56,7 +56,7 @@ class Context(Sendable):
         else:
             self.user_id = data.author.id
             self.user = data.author
-            self.member = data.member or Guild_Member()
+            self.member = data.member or cache.members.get(data.user_id, None) or Guild_Member()
             self.member.user = data.author
             self.is_interaction = False
             self.is_message = True
