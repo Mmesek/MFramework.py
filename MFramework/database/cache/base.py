783f0ee8b8e6e3bcf08c5afc75cb6953bd4ae3bd
Mmesek <13630781+Mmesek@users.noreply.github.com>
2022-05-02 23:55:39 +0200

    Add setting alias & making it regex's pattern

--- MFramework/database/cache/base.py
+++ MFramework/database/cache/base.py
@@ -1,15 +1,19 @@
+import re
+
 from typing import Dict, Set
 
 from MFramework import Snowflake, Guild_Member
 from MFramework.commands import Groups
+
 
 class Base:
     name: str
     color: int
     bot: Guild_Member
     groups: Dict[Groups, Set[Snowflake]]
+
     def __init__(self, *, bot, **kwargs) -> None:
-        self.groups = {i:set() for i in Groups}
+        self.groups = {i: set() for i in Groups}
         self.setBot(bot.user_id)
         if self.bot:
             self.setColor()
@@ -30,18 +34,23 @@ class Base:
             elif role.position > color[0] and color[2] != True:
                 color = (role.position, role.color, False)
         self.color = color[1] if color else None
-    
+
     def calculate_permissions(self):
         for role in self.bot.roles:
             self.permissions |= int(self.roles[role].permissions)
-    
+
     def cachedRoles(self, roles):
         for group in self.groups:
             if any(i in roles for i in self.groups[group]):
                 return group
         return Groups.GLOBAL
 
+
 class Commands(Base):
-    alias: str = '?'
+    alias: re.Pattern
 
     _permissions_set: bool = False
+
+    def __init__(self, *, bot, **kwargs) -> None:
+        self.alias = re.compile(r"|".join([re.escape(bot.alias), re.escape(bot.username), f"{bot.user_id}>"]))
+        super().__init__(bot=bot, **kwargs)
