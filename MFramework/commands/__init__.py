6665cd5cc8fd19ed78796bd03faa7c457d27a26c
Mmesek <13630781+Mmesek@users.noreply.github.com>
2023-11-17 00:40:49 +0100

    Add everyone group & Option to check by int
    Signed-off-by: Mmesek <13630781+Mmesek@users.noreply.github.com>

--- MFramework/commands/__init__.py
+++ MFramework/commands/__init__.py
@@ -9,7 +9,7 @@ Command registery & execution framework
 
 """
 from mlib.types import Enum
-
+from typing import Union
 
 class Groups(Enum):
     SYSTEM = 0
@@ -23,17 +23,18 @@ class Groups(Enum):
     SUPPORTER = 70
     VIP = 80
     GLOBAL = 100
+    EVERYONE = 100
     DM = 200
     MUTED = 210
     LIMBO = 220
 
-    def can_use(cls, value: "Groups") -> bool:
+    def can_use(self, value: Union["Groups", int]) -> bool:
         """Checks if Group is higher or equal than provided one
         Parameters
         ----------
         value:
             Group to compare to. For example, Minimal group that can use"""
-        return cls.value <= value.value
+        return self.value <= (value if type(value) is int else value.value)
 
 
 from .decorators import *  # noqa: F401
