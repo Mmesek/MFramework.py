d9bc356492c97c8e1c80262fb30deff762bbc64e
Mmesek <13630781+Mmesek@users.noreply.github.com>
2024-01-12 18:27:45 +0100

    Format, Remove unnecessary import &Rename Bot mock
    Signed-off-by: Mmesek <13630781+Mmesek@users.noreply.github.com>

--- tests/cache/test_base.py
+++ tests/cache/test_base.py
@@ -1,33 +1,35 @@
 import re
-from MFramework import Bot, Groups
+from MFramework import Groups
 from MFramework.cache.base import Base, Commands, RuntimeCommands, Trigger
 
-class Bot:
+
+class TestBot:
     username = "Test"
     user_id = 1
     alias = "?"
+
 
 def test_cached_roles():
     cache = Base()
 
     assert cache.cached_roles([]) != Groups.ADMIN
 
-    cache.groups = {Groups.ADMIN: {1}, Groups.MODERATOR: {2,3}, Groups.MUTED: {4}, Groups.EVERYONE: {}}
+    cache.groups = {Groups.ADMIN: {1}, Groups.MODERATOR: {2, 3}, Groups.MUTED: {4}, Groups.EVERYONE: {}}
 
-    assert cache.cached_roles([1,2,3]) == Groups.ADMIN
+    assert cache.cached_roles([1, 2, 3]) == Groups.ADMIN
     assert cache.cached_roles([]) == Groups.GLOBAL
     assert cache.cached_roles([]) == Groups.EVERYONE
     assert cache.cached_roles([4]) == Groups.MUTED
 
 
 def test_default_alias():
-    cache = Commands(bot=Bot())
+    cache = Commands(bot=TestBot())
 
     assert cache.alias == re.compile(r"\?|Test|1>")
 
 
 def test_set_alias():
-    bot = Bot()
+    bot = TestBot()
     cache = Commands(bot=bot)
 
     cache.set_alias(bot, "!")
@@ -42,7 +44,7 @@ def test_set_alias():
 
 
 def test_recompile_triggers():
-    cache = RuntimeCommands(bot=Bot())
+    cache = RuntimeCommands(bot=TestBot())
 
     test_1 = Trigger(Groups.ADMIN, "Test", "test trigger", None, None)
     test_2 = Trigger(Groups.ADMIN, "Test2", "test trigger 2", None, None)
@@ -50,14 +52,10 @@ def test_recompile_triggers():
     test_4 = Trigger(Groups.GLOBAL, "Test3", "test_", None, None)
 
     cache.recompile_triggers([test_1, test_2, test_3, test_4])
-    assert cache.triggers == {
-        "Test": test_3,
-        "Test2": test_2,
-        "Test3": test_4
-    }
+    assert cache.triggers == {"Test": test_3, "Test2": test_2, "Test3": test_4}
 
     assert cache.responses == {
         Groups.ADMIN: re.compile(r"(?:(?P<Test>test trigger)|(?P<Test2>test trigger 2))", re.IGNORECASE),
         Groups.NITRO: re.compile(r"(?:(?P<Test>test))", re.IGNORECASE),
-        Groups.GLOBAL: re.compile(r"(?:(?P<Test3>test_))", re.IGNORECASE)
+        Groups.GLOBAL: re.compile(r"(?:(?P<Test3>test_))", re.IGNORECASE),
     }
